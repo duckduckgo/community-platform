f917c30cf1e53776687f0838b612a93c82a83ab4
Torsten Raudssus <github@raudssus.de>
2011-09-07 18:02:04 +0200

    I hate fileendings, i hate fileendings, i hate fileendings

--- lib/DDGC/DB/Result/Token/Context.pm
+++ lib/DDGC/DB/Result/Token/Context.pm
@@ -1,1 +1,47 @@
-package DDGC::DB::Result::Token::Context;use DBIx::Class::Candy -components => [ 'TimeStamp', 'InflateColumn::DateTime', 'InflateColumn::Serializer', 'SingletonRows', 'EncodedColumn' ];table 'token_context';column id => {	data_type => 'bigint',	is_auto_increment => 1,};primary_key 'id';column name => {	data_type => 'text',	is_nullable => 0,};column data => {	data_type => 'text',	is_nullable => 1,	serializer_class => 'YAML',};column notes => {	data_type => 'text',	is_nullable => 1,};column created => {	data_type => 'timestamp with time zone',	set_on_create => 1,};column updated => {	data_type => 'timestamp with time zone',	set_on_create => 1,	set_on_update => 1,};has_many 'tokens', 'DDGC::DB::Result::Token', 'token_context_id';use overload '""' => sub {	my $self = shift;	return 'Token Context '.$self->name.' #'.$self->id;}, fallback => 1;1;
+package DDGC::DB::Result::Token::Context;
+
+use DBIx::Class::Candy -components => [ 'TimeStamp', 'InflateColumn::DateTime', 'InflateColumn::Serializer', 'SingletonRows', 'EncodedColumn' ];
+
+table 'token_context';
+
+column id => {
+	data_type => 'bigint',
+	is_auto_increment => 1,
+};
+primary_key 'id';
+
+column name => {
+	data_type => 'text',
+	is_nullable => 0,
+};
+
+column data => {
+	data_type => 'text',
+	is_nullable => 1,
+	serializer_class => 'YAML',
+};
+
+column notes => {
+	data_type => 'text',
+	is_nullable => 1,
+};
+
+column created => {
+	data_type => 'timestamp with time zone',
+	set_on_create => 1,
+};
+
+column updated => {
+	data_type => 'timestamp with time zone',
+	set_on_create => 1,
+	set_on_update => 1,
+};
+
+has_many 'tokens', 'DDGC::DB::Result::Token', 'token_context_id';
+
+use overload '""' => sub {
+	my $self = shift;
+	return 'Token Context '.$self->name.' #'.$self->id;
+}, fallback => 1;
+
+1;
--- lib/DDGC/DB/Result/Token/Screen.pm
+++ lib/DDGC/DB/Result/Token/Screen.pm
@@ -1,1 +1,42 @@
-package DDGC::DB::Result::Token::Screen;use DBIx::Class::Candy -components => [ 'TimeStamp', 'InflateColumn::DateTime', 'InflateColumn::Serializer', 'SingletonRows', 'EncodedColumn' ];table 'token_screen';column id => {	data_type => 'bigint',	is_auto_increment => 1,};primary_key 'id';column token_id => {	data_type => 'bigint',	is_nullable => 1,};column screen_id => { 	data_type => 'bigint',	is_nullable => 1,};column created => {	data_type => 'timestamp with time zone',	set_on_create => 1,};column updated => {	data_type => 'timestamp with time zone',	set_on_create => 1,	set_on_update => 1,};belongs_to 'token', 'DDGC::DB::Result::Token', 'token_id';belongs_to 'screen', 'DDGC::DB::Result::Screen', 'screen_id';use overload '""' => sub {	my $self = shift;	return 'Token-Screen #'.$self->id;}, fallback => 1;1;
+package DDGC::DB::Result::Token::Screen;
+
+use DBIx::Class::Candy -components => [ 'TimeStamp', 'InflateColumn::DateTime', 'InflateColumn::Serializer', 'SingletonRows', 'EncodedColumn' ];
+
+table 'token_screen';
+
+column id => {
+	data_type => 'bigint',
+	is_auto_increment => 1,
+};
+primary_key 'id';
+
+column token_id => {
+	data_type => 'bigint',
+	is_nullable => 1,
+};
+
+column screen_id => { 
+	data_type => 'bigint',
+	is_nullable => 1,
+};
+
+column created => {
+	data_type => 'timestamp with time zone',
+	set_on_create => 1,
+};
+
+column updated => {
+	data_type => 'timestamp with time zone',
+	set_on_create => 1,
+	set_on_update => 1,
+};
+
+belongs_to 'token', 'DDGC::DB::Result::Token', 'token_id';
+belongs_to 'screen', 'DDGC::DB::Result::Screen', 'screen_id';
+
+use overload '""' => sub {
+	my $self = shift;
+	return 'Token-Screen #'.$self->id;
+}, fallback => 1;
+
+1;
