b45111edc0bd470ba7149d44341ed7b22de5fc41
Torsten Raudssus <github@raudssus.de>
2011-12-03 19:12:50 +0100

    Added feedback form

--- lib/DDGC/Web/Controller/Base.pm
+++ lib/DDGC/Web/Controller/Base.pm
@@ -4,6 +4,8 @@ use namespace::autoclean;
 
 BEGIN {extends 'Catalyst::Controller'; }
 
+use Email::Valid;
+
 sub base :Chained('/base') :PathPart('base') :CaptureArgs(0) {
     my ( $self, $c ) = @_;
 	my $headline_template = $c->req->action;
@@ -22,6 +24,32 @@ sub welcome :Chained('base') :Args(0) {
 
 sub feedback :Chained('base') :Args(0) {
 	my ($self, $c) = @_;
+	delete $c->stash->{headline_template};
+	
+	if ($c->req->params->{submit} && $c->req->params->{feedback}) {
+
+		if ( $c->req->params->{email} && !Email::Valid->address($c->req->params->{email}) ) {
+			$c->stash->{no_valid_email} = 1;
+			$c->stash->{feedback_email} = $c->req->params->{email};
+			$c->stash->{feedback_feedback} = $c->req->params->{feedback};
+			return;
+		}
+		
+		$c->stash->{email} = {
+			to          => 'getty@duckduckgo.com',
+			from        => $c->req->params->{email} ? $c->req->params->{email} : 'noreply@duckduckgo.com',
+			subject     => '[DuckDuckGo Community] New feedback',
+			template	=> 'email/feedback.tt',
+			charset		=> 'utf-8',
+			content_type => 'text/plain',
+		};
+
+		$c->stash->{thanks_for_feedback} = 1;
+
+		$c->forward( $c->view('Email::TT') );
+
+	}
+
 }
 
 __PACKAGE__->meta->make_immutable;
