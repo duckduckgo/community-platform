0ec679f3a9ac44b0a17f63068da66d607668fe7f
Torsten Raudssus <torsten@raudss.us>
2012-05-27 17:31:08 +0200

    Added export to file option

--- script/ddgc_update_locale_dist.pl
+++ script/ddgc_update_locale_dist.pl
@@ -9,14 +9,19 @@ use DDGC;
 use DDGC::LocaleDist;
 use Getopt::Long;
 use File::Copy;
+use Path::Class;
 
 my $domain;
+my $target;
 
 GetOptions(
 	"domain=s" => \$domain,
+	"target=s" => \$target,
 );
 
 die 'you must give a --domain' unless $domain;
+
+$target = dir($target)->absolute if $target;
 
 my $ddgc = DDGC->new;
 my $td = $ddgc->resultset('Token::Domain')->search({ key => $domain },{ prefetch => 'tokens' })->next;
@@ -24,6 +29,12 @@ my $td = $ddgc->resultset('Token::Domain')->search({ key => $domain },{ prefetch
 my $local_dist = DDGC::LocaleDist->new( token_domain => $td );
 my $user = $ddgc->find_user($ddgc->config->duckpan_locale_uploader);
 
-$ddgc->duckpan->add_user_distribution($user,$local_dist->distribution_file);
+if ($target) {
+	print "\nCopying distribution to ".$target."\n";
+	copy($local_dist->distribution_file, file($target,$local_dist->distribution_file->basename));
+} else {
+	print "\nAdding to DuckPAN...\n";
+	$ddgc->duckpan->add_user_distribution($user, $local_dist->distribution_file);
+}
 
 print "\nDone...\n\n";
