8328a4fb636bb29f808bd23c4c4edcbd61171eb3
Torsten Raudssus <torsten@raudss.us>
2012-09-24 05:47:35 +0200

    Removed permission table cause we are stupid

--- lib/DDGC/DB/Result/User.pm
+++ lib/DDGC/DB/Result/User.pm
@@ -99,7 +99,6 @@ has_many 'checked_translations', 'DDGC::DB::Result::Token::Language::Translation
 has_many 'translation_votes', 'DDGC::DB::Result::Token::Language::Translation::Vote', 'users_id';
 has_many 'comments', 'DDGC::DB::Result::Comment', 'users_id';
 has_many 'duckpan_releases', 'DDGC::DB::Result::DuckPAN::Release', 'users_id';
-has_many 'duckpan_permissions', 'DDGC::DB::Result::DuckPAN::Permission', 'users_id';
 has_many 'threads', 'DDGC::DB::Result::Thread', 'users_id';
 
 many_to_many 'languages', 'user_languages', 'language';
--- lib/DDGC/DuckPAN.pm
+++ lib/DDGC/DuckPAN.pm
@@ -38,23 +38,6 @@ sub add_user_distribution {
 	my $dist_data = Dist::Data->new($distribution_filename);
 	my %ret;
 	$ret{error} = 'Only admins may upload so far' unless $user->admin;
-#	for (keys %{$dist_data->packages}) {
-#		my %perms = %{$self->get_namespace_permissions($_)};
-#		if (%perms) {
-#			if (!defined $perms{$user->username}) {
-#				$ret{permission_denied} = {} unless defined $ret{permission_denied};
-#				$ret{permission_denied}->{$_} = \%perms;
-#				$ret{error} = 1;
-#				next;
-#			}
-#		} else {
-#			$self->add_permission($user, $_, 1);
-#			$ret{new_permissions} = [] unless defined $ret{new_permissions};
-#			push @{$ret{new_permissions}}, $_;
-#		}
-#		$ret{namespaces} = [] unless defined $ret{namespaces};
-#		push @{$ret{namespaces}}, $_;
-#	}
 	return \%ret if $ret{error};
 	my @releases = $self->ddgc->db->resultset('DuckPAN::Release')->search({
 		name => $dist_data->name,
@@ -76,27 +59,6 @@ sub add_release {
 		users_id => $user->id,
 		filename => $filename,
 	});
-}
-
-sub add_permission {
-	my ( $self, $user, $namespace, $permission ) = @_;
-	return $self->ddgc->db->resultset('DuckPAN::Permission')->create({
-		namespace => $namespace,
-		users_id => $user->id,
-		permission => $permission,
-	});
-}
-
-sub get_namespace_permissions {
-	my ( $self, $namespace ) = @_;
-	my @permissions = $self->ddgc->db->resultset('DuckPAN::Permission')->search({
-		namespace => $namespace
-	})->all;
-	my %perms;
-	for (@permissions) {
-		$perms{$_->user->username} = $_->permission;
-	}
-	return \%perms;
 }
 
 1;
