16e90c05f0ca72be76ae4e341db72aad545f3ca1
Torsten Raudssus <torsten@raudss.us>
2013-05-13 04:33:26 +0200

    Added blog prototype html, still WIP

--- lib/DDGC/Web/Controller/Blog.pm
+++ lib/DDGC/Web/Controller/Blog.pm
@@ -5,31 +5,34 @@ use namespace::autoclean;
 
 BEGIN {extends 'Catalyst::Controller'; }
 
-use Email::Valid;
-
 sub base :Chained('/base') :PathPart('blog') :CaptureArgs(0) {
-  my ( $self, $c ) = @_;
+	my ( $self, $c ) = @_;
+	push @{$c->stash->{template_layout}}, 'blog/base.tx';
 }
 
-sub do :Chained('base') :CaptureArgs(0) {
-  my ( $self, $c ) = @_;
-}
-
-sub index :Chained('do') :PathPart('index') :Args(0) {
-  my ( $self, $c ) = @_;
-  $c->stash->{entries} = $c->d->blog->list_entries;
+sub index :Chained('base') :PathPart('') :Args(0) {
+	my ( $self, $c ) = @_;
+	#$c->stash->{entries} = $c->d->blog->list_entries;
 }
 
 sub topic :Chained('base') :Args(1) {
-  my ( $self, $c, $topic ) = @_;
-  $c->stash->{entries} = $c->d->blog->get_topical_entries($topic);
-  return $c->go('/default') unless $c->stash->{entries};
+	my ( $self, $c, $topic ) = @_;
+	# $c->stash->{entries} = $c->d->blog->get_topical_entries($topic);
+	# unless ($c->stash->{entries})  {
+	# 	$c->response->redirect($c->chained_uri('Blog','index'));
+	# 	return $c->detach;
+	# }
 }
 
-sub view :Chained('base') :PathPart('') :Args(1) {
-  my ( $self, $c, $blog_entry_url ) = @_;
-  $c->stash->{url} = $blog_entry_url;
-  $c->stash->{entry} = $c->d->blog->list_entry($blog_entry_url);
+sub post_base :Chained('base') :PathPart('') :CaptureArgs(1) {
+	my ( $self, $c, $post ) = @_;
+	#$c->stash->{post} = $c->d->blog->list_entry($post);
+}
+
+sub post :Chained('post_base') :PathPart('') :Args(0) {
+	my ( $self, $c, $blog_entry_url ) = @_;
+	#$c->stash->{url} = $blog_entry_url;
+	#$c->stash->{entry} = $c->d->blog->list_entry($blog_entry_url);
 }
 
 1;
