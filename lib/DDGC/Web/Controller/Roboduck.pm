e12bc6fdb02c37bad61865c5558e79656aeebe1e
Torsten Raudssus <torsten@raudss.us>
2013-05-17 01:40:11 +0200

    Added JSON endpoint for Roboduck

--- lib/DDGC/Web/Controller/Roboduck.pm
+++ lib/DDGC/Web/Controller/Roboduck.pm
@@ -5,18 +5,26 @@ use namespace::autoclean;
 BEGIN {extends 'Catalyst::Controller'; }
 
 sub base :Chained('/base') :PathPart('roboduck') :CaptureArgs(0) {
-    my ( $self, $c ) = @_;
+	my ( $self, $c ) = @_;
+	my $query = $c->req->param('q');
+	if ($query) {
+		my ( $answer, $id ) = $c->d->roboduck->tell(
+			$query, $c->user ? $c->user->username : 'Stranger'
+		);
+		$c->stash->{answer} = $answer;
+	}
 }
 
 sub index :Chained('base') :PathPart('') :Args(0) {
-    my ( $self, $c ) = @_;
-    my $query = $c->req->param('q');
-    if ($query) {
-    	my ( $answer, $id ) = $c->d->roboduck->tell(
-    		$query, $c->user ? $c->user->username : 'Stranger'
-    	);
-    	$c->stash->{answer} = $answer;
-    }
+	my ( $self, $c ) = @_;
+}
+
+sub json :Chained('base') :Args(0) {
+	my ( $self, $c ) = @_;
+	$c->stash->{x} = {
+		answer => $c->stash->{answer},
+	};
+	$c->forward( $c->view('JSON') );
 }
 
 __PACKAGE__->meta->make_immutable;
