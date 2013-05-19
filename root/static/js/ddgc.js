e2fd92ca7d9797ebba45a404170cbf3a4d7b0b8c
Doug Brown <sdougbrown@gmail.com>
2013-05-19 14:08:04 -0400

    improved translation user flow (js fixes), minor css touchup

--- root/static/js/ddgc.js
+++ root/static/js/ddgc.js
@@ -19,17 +19,19 @@ $(document).ready(function() {
 	});
 	
 	$('.hide-translations').click(function(e) {
-		$(this).parent().addClass('hide').siblings('.current-translations-min').removeClass('hide');
+		$(this).parent().addClass('hide').siblings('.current-translations-min').removeClass('hide');		
 		e.preventDefault();
 	});
 	$('.add-translations').click(function(e) {
 		$(this).parent().addClass('hide').siblings('.current-translations-min').removeClass('hide');
-		$(this).parents('fieldset.row').children('.user-translation').removeClass('hide');
+		$(this).parents('fieldset.row').children('.user-translation').removeClass('js-hide');
+		$('.token-submit').removeClass('js-hide');
 		e.preventDefault();
 	});
 	$('.show-translations').click(function(e) {
 		$(this).parent().addClass('hide').siblings('.current-translations').removeClass('hide');
-		$(this).parents('fieldset.row').children('.user-translation').addClass('hide');
+		$(this).parents('fieldset.row').children('.user-translation').addClass('js-hide');
+		$('.token-submit').addClass('js-hide');
 		e.preventDefault();
 	});
 	// Nested Comment toggle
