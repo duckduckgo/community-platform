b5df12312619a941a168e337fb2c4bf5c6aae9b8
Torsten Raudssus <torsten@raudss.us>
2013-06-10 01:10:32 +0200

    .... and changed it ;)

--- lib/DDGC/Web/Controller/Userpage.pm
+++ lib/DDGC/Web/Controller/Userpage.pm
@@ -12,27 +12,27 @@ sub base :Chained('/base') :PathPart('') :CaptureArgs(0) {
 }
 
 sub user :Chained('base') :PathPart('') :CaptureArgs(1) {
-    my ( $self, $c, $username ) = @_;
-    my $json = 0;
-    if ($username =~ m/^(.*)\.json$/) {
-        $username = $1;
-        $json = 1;
-    }
-    $c->stash->{user} = $c->d->find_user($username);
-    $c->stash->{up} = DDGC::User::Page->new_from_user($c->stash->{user});
-    $c->stash->{fields} = $c->stash->{up}->attribute_fields;
-    unless ($c->stash->{user} && $c->stash->{user}->public) {
-    	return $c->go('/default');
-    }
-    $c->stash->{x} = $c->stash->{up}->data;
-    $c->add_bc('User page of '.$username, '');
-    $c->stash->{title} = $username." User Page";
-    $c->forward('View::JSON') if $json;
+	my ( $self, $c, $username ) = @_;
+	$c->stash->{user} = $c->d->find_user($username);
+	$c->stash->{up} = DDGC::User::Page->new_from_user($c->stash->{user});
+	$c->stash->{fields} = $c->stash->{up}->attribute_fields;
+	unless ($c->stash->{user} && $c->stash->{user}->public) {
+		return $c->go('/default');
+	}
+	$c->stash->{x} = $c->stash->{up}->data;
+	$c->stash->{x}->{username} = $username;
+	$c->add_bc('User page of '.$username, '');
+	$c->stash->{title} = $username." User Page";
 }
 
 sub home :Chained('user') :PathPart('') :Args(0) {
+	my ( $self, $c ) = @_;
+	$c->stash->{last_comments} = $c->stash->{user}->last_comments(1,3);
+}
+
+sub json :Chained('user') :Args(0) {
     my ( $self, $c ) = @_;
-    $c->stash->{last_comments} = $c->stash->{user}->last_comments(1,3);
+	$c->forward('View::JSON');
 }
 
 __PACKAGE__->meta->make_immutable;
