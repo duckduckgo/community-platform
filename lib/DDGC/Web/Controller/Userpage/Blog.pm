c2a65ff739d6d128515021d21745072c6420aa39
Torsten Raudssus <torsten@raudss.us>
2013-06-30 23:41:03 +0200

    Hups!

--- lib/DDGC/Web/Controller/Userpage/Blog.pm
+++ lib/DDGC/Web/Controller/Userpage/Blog.pm
@@ -13,7 +13,6 @@ sub base :Chained('/userpage/base') :PathPart('blog') :CaptureArgs(0) {
 	$c->stash->{blog_resultset} = $c->stash->{user}->search_related('user_blogs',{
 		($c->user && $c->user->id == $c->stash->{user}->id) ? () : ( live => 1 ),
 	});
-	$c->stash->{blog_controller} = 'Userpage::Blog';
 	$c->add_bc($c->stash->{user}->username.' Blog', $c->chained_uri('Userpage::Blog', 'index',$c->stash->{user}->username));
 }
 
