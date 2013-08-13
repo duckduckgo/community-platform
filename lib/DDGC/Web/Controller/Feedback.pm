ba9b8ffe4cdddf6c4428d35cd9cb420ba25b1675
Torsten Raudssus <torsten@raudss.us>
2013-08-13 07:55:59 +0200

    Feedback system, still non option field handling missing, still js case missing

--- lib/DDGC/Web/Controller/Feedback.pm
+++ lib/DDGC/Web/Controller/Feedback.pm
@@ -8,7 +8,10 @@ BEGIN {extends 'Catalyst::Controller'; }
 
 sub base :Chained('/base') :PathPart('feedback') :CaptureArgs(0) {
   my ( $self, $c ) = @_;
+  $c->add_bc('Feedback');
 }
+
+sub thanks :Chained('base') :Args(0) {}
 
 sub index_redirect :Chained('base') :PathPart('') :Args(0) {
   my ( $self, $c ) = @_;
@@ -41,26 +44,81 @@ sub step :Chained('feedback') :PathPart('') :Args(1) {
     $previous_step = $current_step;
     $current_step = $current_step->options->[$step_index]->target;
   }
-  my $session_key = join('-',@steps).'/'.$c->stash->{feedback_name};
+  my $session_key = $c->stash->{feedback_name}.'/'.join('-',@steps);
+  my $submit;
   for (keys %{$c->req->params}) {
     if ($_ =~ m/^option_(\d+)$/) {
       my $next_step_index = $1;
       $previous_step = $current_step;
-      $current_step = $current_step->options->[$next_step_index]->target;
-      push @steps, $next_step_index;
+      my $next_option = $current_step->options->[$next_step_index];
+      if ($next_option->type eq 'submit') {
+        $submit = 1;
+      } else {
+        $current_step = $next_option->target;
+        push @steps, $next_step_index;
+      }
     } elsif ($_ =~ m/^step_back$/) {
       $current_step = $previous_step;
       pop @steps;
     } else {
-      $c->session->{$session_key} = {} unless defined $c->session->{$session_key};
-      $c->session->{$session_key}->{$_} = $c->req->param($_);
+      $c->session->{feedback} = {} unless defined $c->session->{feedback};
+      $c->session->{feedback}->{$session_key} = {} unless defined $c->session->{feedback}->{$session_key};
+      $c->session->{feedback}->{$session_key}->{$_} = $c->req->param($_);
     }
   }
   $c->stash->{step_count} = scalar @steps;
   $c->stash->{steps_arg} = '-'.join('-',@steps);
   $c->stash->{steps} = \@steps;
   $c->stash->{step} = $current_step;
+  if ($submit) {
+    my %data;
+    my $data_step = $c->stash->{feedback};
+
+    my @data_steps;
+    my $i = 1;
+    for (@steps) {
+      push @data_steps, $_;
+      my $step_session_key = $c->stash->{feedback_name}.'/'.join('-',@data_steps);
+      if (defined $c->session->{feedback}->{$step_session_key}) {
+        my %step_data = %{$c->session->{feedback}->{$step_session_key}};
+        for (keys %step_data) {
+          my $data_key = $i.'_'.$_;
+          $data{$data_key} = $step_data{$_};
+        }
+      }
+      my $next_option = $data_step->options->[$_];
+      $data{$i} = $next_option->description;
+      $i++;
+      if ($next_option->type eq 'submit') {
+        last;
+      } else {
+        $data_step = $next_option->target;
+      }
+    }
+
+    $c->stash->{feedback_data} = \%data;
+    my @header_field_names = $c->req->headers->header_field_names();
+    $c->stash->{header_field_names} = [grep { $_ ne 'COOKIE' } @header_field_names];
+
+    $c->stash->{email} = {
+      to          => 'communitytest@duckduckgo.com',
+      from        => 'noreply@dukgo.com',
+      subject     => '[DDG Feedback '.$c->stash->{feedback_name}.'] '.$data{'1'},
+      template        => 'email/feedback.tx',
+      charset         => 'utf-8',
+      content_type => 'text/html',
+    };
+
+    $c->forward( $c->view('Email::Xslate') );
+
+    delete $c->session->{feedback};
+
+    $c->res->redirect($c->chained_uri('Feedback','thanks'));
+    return $c->detach;
+  }
 }
+
+
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
