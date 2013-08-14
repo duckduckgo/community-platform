37e9ab34371078e55231894a321e06a6b45ab307
Doug Brown <doug@duckduckgo.com>
2013-08-14 18:14:33 -0400

    icon assignment to feedback objects

--- lib/DDGC/Feedback/Config/Bug.pm
+++ lib/DDGC/Feedback/Config/Bug.pm
@@ -8,56 +8,56 @@ sub feedback_title { 'I need to report a bug!' }
 sub feedback {[
   { description => "It's a bug on mobile (e.g. phone, tablet, media device)", icon => "phone" },
       bug_mobile(),
-  "It's a bug on Desktop (good ole' fashion computers)",
+  { description => "It's a bug on Desktop (good ole' fashion computers)", icon => "browser" },
       bug_desktop(),
 ]}
 
 sub bug_mobile {[
-  "The bug is in the DuckDuckGo app",
+  { description => "The bug is in the DuckDuckGo app", icon => "dax" },
       "Please report the bug through the DuckDuckGo app by going to the Settings menu --&gt; Give Feedback. That way you don't have to type out your app version and details!",
-  "The bug is with DuckDuckGo in a 3rd party app or mobile browser (e.g. Safari, iCab, Dolphin)",
+  { description => "The bug is with DuckDuckGo in a 3rd party app or mobile browser (e.g. Safari, iCab, Dolphin)", icon => "ddg-browser" },
       bug_mobile_thirdparty(),
 ]}
 
 sub bug_mobile_thirdparty {[
-  { description => "I have an Android device", icon => "phone" },
+  { description => "I have an Android device", icon => "android" },
       bug_mobile_thirdparty_android(),
-  { description => "I have an iOS device", icon => "phone" },
+  { description => "I have an iOS device", icon => "apple" },
       bug_mobile_thirdparty_ios(),
-  { description => "I have a Windows mobile device", icon => "phone" },
+  { description => "I have a Windows mobile device", icon => "windows8" },
       bug_mobile_thirdparty_windows(),
   { description => "I have a different type of mobile device than those listed above", icon => "phone" },
       bug_mobile_thirdparty_other(),
 ]}
 
 sub bug_desktop {[
-  "It’s a problem with the DuckDuckGo site",
+  { description => "It’s a problem with the DuckDuckGo site", icon => "dax" },
       bug_desktop_site(),
-  "It’s a problem with a DuckDuckGo browser addon",
+  { description => "It’s a problem with a DuckDuckGo browser addon", icon => "ddg-browser" },
       bug_desktop_addon(),
 ]}
 
 sub bug_desktop_site {[
-  "I have a Windows computer",
+  { description => "I have a Windows computer", icon => "windows8" },
       bug_desktop_site_windows(),
-  "I have a Mac computer",
+  { description => "I have a Mac computer", icon => "finder" },
       bug_desktop_site_mac(),
-  "I have a Linux-based computer",
+  { description => "I have a Linux-based computer", icon => "tux" },
       bug_desktop_site_linux(),
-  "I have a different type of computer than those listed above",
+  { description => "I have a different type of computer than those listed above", icon => "dax" },
       bug_desktop_site_other(),
 ]}
 
 sub bug_desktop_addon {[
-  "I’m using your Firefox addon",
+  { description => "I’m using your Firefox addon", icon => "firefox" },
       bug_desktop_addon_firefox(),
-  "I’m using your Chrome addon",
+  { description => "I’m using your Chrome addon", icon => "chrome" },
       bug_desktop_addon_chrome(),
-  "I’m using your Safari addon",
+  { description => "I’m using your Safari addon", icon => "safari" },
       bug_desktop_addon_safari(),
-  "I’m using your Internet explorer addon",
+  { description => "I’m using your Internet explorer addon", icon => "IE" },
       bug_desktop_addon_ie(),
-  "I’m using your Opera addon",
+  { description => "I’m using your Opera addon", icon => "opera" },
       bug_desktop_addon_opera(),
 ]}
 
@@ -65,39 +65,39 @@ sub bug_desktop_addon {[
 #-----------------------------------------------------------------
 
 sub include_bug {
-  { name => 'bug', description => "The bug is", type => "textarea", },
-  { name => 'email', description => "Your email (not required)", type => "email", },
-  { name => 'bug_steps', description => "You can reproduce it by", type => "textarea", optional => 1, placeholder => "Step-by-step instructions please"},
-  { name => 'bug_other', description => $_[0] || "Other helpful info", type => "textarea", optional => 1, placeholder => 'e.g. only happens when the moon is waxing and you have underwear on your head.', },
-  "Submit bugreport",
+  { name => 'bug', description => "The bug is", type => "textarea", icon => "bug",},
+  { name => 'email', description => "Your email (not required)", type => "email", icon => "inbox",},
+  { name => 'bug_steps', description => "You can reproduce it by", type => "textarea", optional => 1, placeholder => "Step-by-step instructions please", icon => "coffee"},
+  { name => 'bug_other', description => $_[0] || "Other helpful info", type => "textarea", optional => 1, placeholder => 'e.g. only happens when the moon is waxing and you have underwear on your head.', icon => "folder",},
+  "Submit Bug Report",
 }
 
 #-----------------------------------------------------------------
 
 sub include_bug_app {
-  { name => 'bug_app', description => "The app with the bug is called", type => "text", placeholder => "e.g. Upset Ducks"},
+  { name => 'bug_app', description => "The app with the bug is called", type => "text", placeholder => "e.g. Upset Ducks", icon => "windows"},
 }
 
 sub bug_mobile_thirdparty_android {[
-  { name => 'android_mobile', description => 'My device is a', type => "text", placeholder => "e.g. Samsung Galaxy S3, HTC One" },
+  { name => 'android_mobile', description => 'My device is a', type => "text", placeholder => "e.g. Samsung Galaxy S3, HTC One", icon => "android" },
   include_bug_app(),
   include_bug(),
 ]}
 
 sub bug_mobile_thirdparty_ios {[
-  { name => 'ios_mobile', description => 'My device is a', type => "text", placeholder => "e.g. Apple iPhone 5, iPad" },
+  { name => 'ios_mobile', description => 'My device is a', type => "text", placeholder => "e.g. Apple iPhone 5, iPad", icon => "apple" },
   include_bug_app(),
   include_bug(),
 ]}
 
 sub bug_mobile_thirdparty_windows {[
-  { name => 'windows_mobile', description => 'My device is a', type => "text", placeholder => "e.g. Nokia Lumia 1020, Microsoft Surface Pro" },
+  { name => 'windows_mobile', description => 'My device is a', type => "text", placeholder => "e.g. Nokia Lumia 1020, Microsoft Surface Pro", icon => "windows8" },
   include_bug_app(),
   include_bug(),
 ]}
 
 sub bug_mobile_thirdparty_other {[
-  { name => 'other_mobile', description => 'My device is a', type => "text", placeholder => "e.g. Blackberry Z10, Kivo tablet" },
+  { name => 'other_mobile', description => 'My device is a', type => "text", placeholder => "e.g. Blackberry Z10, Kivo tablet", icon => "phone" },
   include_bug_app(),
   include_bug(),
 ]}
@@ -155,37 +155,37 @@ sub include_addon_os {
 }
 
 sub include_addon_version {
-  { name => $_[0].'_version', description => "My browser version is", type => "text", placeholder => $_[1], optional => 1 }
+  { name => $_[0].'_version', description => "My browser version is", type => "text", placeholder => $_[1], $_[2] => $_[3], optional => 1 }
 }
 
 sub bug_desktop_addon_firefox {[
   include_addon_os(),
-  include_addon_version( firefox => "e.g. 21, 22, 23" ),
+  include_addon_version( firefox => "e.g. 21, 22, 23", icon => "firefox" ),
   include_bug_desktop(),
 ]}
 
 sub bug_desktop_addon_chrome {[
   include_addon_os(),
-  include_addon_version( chrome => "e.g. 28.0.1500.95" ),
+  include_addon_version( chrome => "e.g. 28.0.1500.95", icon => "chrome" ),
   include_bug_desktop(),
 ]}
 
 sub bug_desktop_addon_safari {[
   include_addon_os(),
-  include_addon_version( safari => "e.g. 6.0.5" ),
+  include_addon_version( safari => "e.g. 6.0.5", icon => "safari" ),
   include_bug_desktop(),
 ]}
 
 sub bug_desktop_addon_ie {[
   include_addon_os(),
-  include_addon_version( firefox => "e.g. 7, 8, 9" ),
+  include_addon_version( firefox => "e.g. 7, 8, 9", icon => "IE" ),
   include_bug_desktop(),
 ]}
 
-sub bug_desktop_addon_opera {[
+sub bug_desktop_addon_opera {[ 
   include_addon_os(),
-  include_addon_version( opera => "" ),
-  include_bug_desktop(),
+  include_addon_version( opera => "", icon => "opera" ),
+  include_bug_desktop(),  
 ]}
 
 #-----------------------------------------------------------------
