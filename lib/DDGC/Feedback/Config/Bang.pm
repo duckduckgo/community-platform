ab71e231abdbf809e6316df5a26aab4bcbf0ce68
Doug Brown <doug@duckduckgo.com>
2013-08-15 12:17:17 -0400

    improved UI for Bang and Feature feedback

--- lib/DDGC/Feedback/Config/Bang.pm
+++ lib/DDGC/Feedback/Config/Bang.pm
@@ -6,12 +6,16 @@ use warnings;
 sub feedback_title { 'I’d like to submit a new !bang or report a broken !bang.' }
 
 sub feedback {[
-  { description => 'To submit new !bangs, use this page: <a href="https://duckduckgo.com/newbang">https://duckduckgo.com/newbang</a>.', type => "info" },
-  { description => 'To let us know about a broken <a href="https://duckduckgo.com/bang.html">!bang</a>, please use this guide:', type => "info" },
-  { name => "bang", description => "The !bang that doesn’t work is", type => "text", placeholder => "e.g. !wikipedia, !so, !ebay" },
-  { name => "issue", description => "The issue is that", type => "text", placeholder => "e.g. the site returns an error, the site no longer exists" },
-  { name => 'email', description => "Your email (not required)", type => "email", },
-  "Report broken !bang",
+  { description => 'To submit new <a href="https://duckduckgo.com/bang.html">!bangs</a>, use this page: <a href="https://duckduckgo.com/newbang">https://duckduckgo.com/newbang</a>.', type => "info", icon => "newspaper" },
+  { description => 'Let us know about a broken !bang', icon => "bang" },
+    broken_bang(),
+]}
+
+sub broken_bang {[
+  { name => "bang", description => "The !bang that doesn’t work is", type => "text", placeholder => "e.g. !wikipedia, !so, !ebay", icon => "bang" },
+  { name => "issue", description => "The issue is that", type => "text", placeholder => "e.g. the site returns an error, the site no longer exists", icon => "bug" },
+  { name => 'email', description => "Your email (not required)", type => "email", icon => "inbox", optional => 1},
+  { name => 'submit', description => "Report broken !bang", cssclass => "fb-step--submit", icon => "mail" }
 ]}
 
 1;
--- lib/DDGC/Feedback/Config/Feature.pm
+++ lib/DDGC/Feedback/Config/Feature.pm
@@ -6,12 +6,12 @@ use warnings;
 sub feedback_title { "I've got a feature request." }
 
 sub feedback {[
-  { description => "We really appreciate you taking the time to offer your suggestions. Some of the common ones we get are to make an <a href='http://help.dukgo.com/customer/portal/articles/215614-email'>email service</a>, <a href='http://help.dukgo.com/customer/portal/articles/907725-adding-duckduckgo-to-your-browser'>a browser</a>, and for things like image and map search (which we're working on). If you haven't found what you're looking for in the <a href='http://help.dukgo.com/'>Help pages</a>, please use the guide below to tell us what you'd like to see in DuckDuckGo:", type => "info" },
-  { description => "It's a feature that could be an instant answer" },
+  { description => "We really appreciate you taking the time to offer your suggestions. Some of the common ones we get are to make an <a href='http://help.dukgo.com/customer/portal/articles/215614-email'>email service</a>, <a href='http://help.dukgo.com/customer/portal/articles/907725-adding-duckduckgo-to-your-browser'>a browser</a>, and for things like image and map search (which we're working on). If you haven't found what you're looking for in the <a href='http://help.dukgo.com/'>Help pages</a>, please use the guide below to tell us what you'd like to see in DuckDuckGo:", type => "info", icon => "newspaper", },
+  { description => "It's a feature that could be an instant answer", icon => "sun" },
     "Please submit instant answer ideas to our community voting platform at <a href='http://ideas.duckduckhack.com/'>http://ideas.duckduckhack.com/</a>. If you're a developer, you can even make them yourself at <a href='http://duckduckhack.com/'>http://duckduckhack.com/</a>. For some inspiration, check out the current instant answers here <a href='https://duckduckgo.com/goodies'>https://duckduckgo.com/goodies</a>.",
-  { description => "It's a feature that wouldn’t work as an instant answer" },
+  { description => "It's a feature that wouldn’t work as an instant answer", icon => "coffee" },
     "Please submit your idea to our community forum so that others can help expand (or even develop) your idea: <a href='https://duck.co/'>https://duck.co/</a>",
-  { description => "It's a translation request" },
+  { description => "It's a translation request", icon => "globe" },
     "You can translate DuckDuckGo to your language through this <a href='https://dukgo.com/translate'>community platform</a>",
     "" # workaround for non submittable ending points like this here.
 ]}
