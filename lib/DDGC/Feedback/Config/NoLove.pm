5313dcae7fb3c2835428463ade0e3762f97ae796
Doug Brown <doug@duckduckgo.com>
2013-08-15 16:31:10 -0400

    usability improvements for feedback form, improved thanks page

--- lib/DDGC/Feedback/Config/NoLove.pm
+++ lib/DDGC/Feedback/Config/NoLove.pm
@@ -9,7 +9,7 @@ sub feedback {[
   { description => "We're always looking for ways to improve and we really appreciate you reaching out with your suggestions. Please let us know how we can do better by dropping us a short description in the box below:", type => "info", icon => "newspaper" },
   { name => "no_love", description => "I don't love", placeholder => "We _can_ handle the truth!", type => "textarea", icon => "trash" },
   { name => 'email', description => "Your email (not required)", placeholder => "We'd like to get back to you, but you can leave this blank.", type => "email", icon => "inbox", optional => 1, },  
-  { name => 'submit', description => "Send Feedback", icon => "mail", cssclass => "fb-step--submit" }
+  { description => "Send Feedback", icon => "mail", cssclass => "fb-step--submit", type => "submit" }
 ]}
 
 1;
