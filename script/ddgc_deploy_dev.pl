dc87bc42c3baeedaf9ff7d3ad7c163a58ad4b47b
Wallace Reis <wallace@reis.me>
2013-10-10 18:10:14 +0000

    Dev deployment: extend db truncation
    In case of pg client apps are missing (dropdb, createdb, etc), let's
    connect to the default and proceed with truncation through DBI.

--- script/ddgc_deploy_dev.pl
+++ script/ddgc_deploy_dev.pl
@@ -12,6 +12,8 @@ use DDGC;
 use DDGCTest::Database;
 use File::Path qw( make_path remove_tree );
 use String::ProgressBar;
+use POSIX;
+use DBI;
 
 use Getopt::Long;
 
@@ -33,8 +35,20 @@ if (-d $config->rootdir_path) {
       if ($config->db_dsn =~ m/(?:database|dbname)=([\w\d]+)/) {
         my $db = $1;
         my $userarg = length($config->db_user) ? "-U ".$config->db_user : "";
+	print "Truncating database...\n";
         system("dropdb $userarg ".$db);
-        system("createdb $userarg ".$db);
+	if ( !WIFEXITED(${^CHILD_ERROR_NATIVE}) ) {
+	    my $dsn = $config->db_dsn;
+	    $dsn =~ s/(database|dbname)=[\w\d]+/$1=postgres/;
+	    my $dbh = DBI->connect(
+		$dsn, $config->db_user, $config->db_password
+	    );
+	    $dbh->do("DROP DATABASE $db") or die $dbh->errstr;
+	    $dbh->do("CREATE DATABASE $db") or die $dbh->errstr;
+        }
+        else {
+	    system("createdb $userarg ".$db);
+	}
       } else {
         die "Can't find out your db name from DSN";
       }
