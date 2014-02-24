14e8eec782a9f9618ef8f3d4e83aa295b1ed7733
Michael Smith <crazedpsyc@duckduckgo.com>
2014-02-24 15:51:28 -0700

    Re-enable GitHub integration for development.
    This reverts commit c2ed78bc3e39e0ef3f23eba1e26eabc37b53f7d3.

--- lib/DDGC.pm
+++ lib/DDGC.pm
@@ -11,6 +11,7 @@ use DDGC::Markup;
 use DDGC::Envoy;
 use DDGC::Postman;
 use DDGC::Stats;
+use DDGC::GitHub;
 use DDGC::Forum;
 use DDGC::Util::DateTime;
 
@@ -152,12 +153,12 @@ has stats => (
 );
 sub _build_stats { DDGC::Stats->new({ ddgc => shift }) }
 
-#has github => (
-#	isa => 'DDGC::GitHub',
-#	is => 'ro',
-#	lazy_build => 1,
-#);
-#sub _build_github { DDGC::GitHub->new({ ddgc => shift }) }
+has github => (
+	isa => 'DDGC::GitHub',
+	is => 'ro',
+	lazy_build => 1,
+);
+sub _build_github { DDGC::GitHub->new({ ddgc => shift }) }
 
 has cache => (
 	isa => 'Cache::Cache',
