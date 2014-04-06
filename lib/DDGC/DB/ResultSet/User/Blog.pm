3e8dc15e2cdd8d1bc9c4013d5ea5d3c7ddfaf4d3
Michael Smith <crazedpsyc@duckduckgo.com>
2014-04-06 16:29:32 -0600

    Fix blog sorting

--- lib/DDGC/DB/ResultSet/User/Blog.pm
+++ lib/DDGC/DB/ResultSet/User/Blog.pm
@@ -40,8 +40,8 @@ sub values_via_form {
 }
 
 sub most_recent_updated {
-  my $self = shift;
-  return $self->order_by({ -desc => $self->me . 'updated' });
+	my $self = shift;
+	return $self->order_by({ -desc => $self->me . 'updated' });
 }
 
 sub live {
@@ -58,7 +58,8 @@ sub posts_by_day {
 	for (@posts) {
 		my $day = $_->date->strftime('%d');
 		my $month = $_->date->strftime('%m');
-		my $id = $month.$day + 0;
+		my $year = $_->date->strftime('%Y');
+		my $id = $year.$month.$day + 0;
 		$days{$id} = [] unless defined $days{$id};
 		push @{$days{$id}}, $_;
 	}
@@ -66,52 +67,52 @@ sub posts_by_day {
 }
 
 sub company_blog {
-  my ( $self, $user ) = @_;
-  return $self->search({
-    $self->me . 'company_blog' => 1,
-    $user && $user->admin ? () : ( $self->me . 'live' => 1 ),
-  });
+	my ( $self, $user ) = @_;
+	return $self->search({
+		$self->me . 'company_blog' => 1,
+		$user && $user->admin ? () : ( $self->me . 'live' => 1 ),
+	});
 }
 
 sub metadata {
-  my $self = shift;
-  my (@topics,$archives);
-  for ($self->all) {
-    push @topics, @{$_->topics||[]};
-    my $created = $_->created;
-    $archives->{$created->strftime('%Y-%m')} = $created;
-  }
-  return {
-    topics => [ sort { lc($a) cmp lc($b) } uniq @topics ],
-    archives => $archives,
-  };
+	my $self = shift;
+	my (@topics,$archives);
+	for ($self->all) {
+		push @topics, @{$_->topics || []};
+		my $created = $_->created;
+		$archives->{$created->strftime('%Y-%m')} = $created;
+	}
+	return {
+		topics => [ sort { lc($a) cmp lc($b) } uniq @topics ],
+		archives => $archives,
+	};
 }
 
 sub filter_by_date {
-  my ( $self, $dt ) = @_;
-  my ( $year, $month ) = split(/\-/, $dt);
-  $dt = DateTime->new(year => $year, month => $month);
-  my $dt_parser = $self->schema->storage->datetime_parser;
-  return $self->search({
-    ($self->me . 'created') => {
-      -between => [
-        $dt_parser->format_datetime($dt),
-        $dt_parser->format_datetime(
-          $dt->clone->add(months => 1)->subtract(days => 1)
-        ),
-      ],
-    },
-  });
+	my ( $self, $dt ) = @_;
+	my ( $year, $month ) = split(/\-/, $dt);
+	$dt = DateTime->new(year => $year, month => $month);
+	my $dt_parser = $self->schema->storage->datetime_parser;
+	return $self->search({
+	($self->me . 'created') => {
+		-between => [
+	$dt_parser->format_datetime($dt),
+	$dt_parser->format_datetime(
+		$dt->clone->add(months => 1)->subtract(days => 1)
+	),
+		],
+	},
+	});
 }
 
 sub util_json_string { JSON->new->allow_nonref(1)->encode(shift) }
 
 sub filter_by_topic {
-  my ( $self, $topic ) = @_;
-  $topic = util_json_string($topic);
-  return $self->search({
-      $self->me . 'topics' => { like => sprintf('%%%s%%', $topic) }
-  });
+	my ( $self, $topic ) = @_;
+	$topic = util_json_string($topic);
+	return $self->search({
+		$self->me . 'topics' => { like => sprintf('%%%s%%', $topic) }
+	});
 }
 
 no Moose;
--- lib/DDGC/Web/Controller/Blog.pm
+++ lib/DDGC/Web/Controller/Blog.pm
@@ -10,11 +10,11 @@ sub index_title { 'Latest DuckDuckGo Blog posts' }
 
 sub base :Chained('/base') :PathPart('blog') :CaptureArgs(0) {
 	my ( $self, $c ) = @_;
-  $c->stash->{title} = 'DuckDuckGo Blog';
+	$c->stash->{title} = 'DuckDuckGo Blog';
 	push @{$c->stash->{template_layout}}, 'blog/base.tx';
 	$c->stash->{page_class} = "page-blog texture";
 	$c->stash->{blog_resultset} = $c->d->rs('User::Blog')->company_blog(
-	    $c->user_exists ? $c->user : undef
+		$c->user_exists ? $c->user : undef
 	);
 	$c->add_bc('DuckDuckGo Blog posts', $c->chained_uri('Blog', 'index'));
 }
