75dd8e69eabcc1b73421ca1ee3a7fb9cfa932a6a
John Barrett <john@jbrt.org>
2014-08-12 15:52:11 -0400

    Script to purge moderated responses from database.

---
+++ script/ddgc_clear_modded_content.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+
+use FindBin;
+use lib $FindBin::Dir . "/../lib";
+
+use strict;
+use warnings;
+
+use DDGC;
+use Getopt::Long;
+use DateTime;
+
+my $doit;
+
+GetOptions ("really-delete-comments", \$doit);
+
+my $d = DDGC->new;
+sub format_datetime { $d->db->storage->datetime_parser->format_datetime(shift) };
+
+my $comments = $d->rs('Comment')->search_rs(
+    {
+        ghosted     => 1,
+        checked     => { '!=' => undef },
+        created     => { '<' => format_datetime( DateTime->now - DateTime::Duration->new( hours => 1 ) )  },
+        parent_id   => { '!=' => undef },,
+        context     => 'DDGC::DB::Result::Thread',
+    },
+    {},
+);
+
+if (my $no_comments = $comments->count) {
+    if ($doit) {
+        my $no_comments = $comments->count;
+        $comments->delete;
+        print "Deleted $no_comments comments\n";
+    }
+    else {
+        while (my $comment = $comments->next) {
+            print $comment->id . ": " . $comment->content . " by " . $comment->user->username . "\n";
+        }
+        print "\n\nRun with --really-delete-comments to delete these comments\n";
+    }
+}
+
