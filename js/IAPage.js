18b509b56fc11749952c4a7bc610dcb8aab7fe61
Russell Holt <russell@duckduckgo.com>
2014-10-13 02:23:29 -0400

    separate index and page

---
+++ js/IAPage.js
@@ -0,0 +1,22 @@
+(function(env) {
+
+
+    // placeholder
+
+    DDH.IAPage = function(ops) { };
+
+    // this could get the single IA json like the index.
+    // but for now the page is being built with xslate
+    DDH.IAPage.prototype = {
+        init: function() {
+            console.log("IAPage.init()\n"); 
+
+            if (DDH_iaid) {
+                console.log("for ia id '%s'", DDH_iaid);
+                //$.getJSON("/ia/json/view/" + DDH_iaid, function(x) {
+                //}
+            }
+        }
+    };
+
+})(DDH);
