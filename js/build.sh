5accb6586435bcb622d4468094156776407115b0
Russell Holt <russell@duckduckgo.com>
2014-10-13 02:24:34 -0400

    uses named js files in order

--- js/build.sh
+++ js/build.sh
@@ -5,5 +5,5 @@ INST="../root/static/js"
 echo building $INST/ia.js
 
 handlebars -f handlebars_tmp *.handlebars
-cat handlebars_tmp *.js > $INST/ia.js
+cat handlebars_tmp DDH.js IAIndex.js IAPage.js ready.js > $INST/ia.js
 rm handlebars_tmp
