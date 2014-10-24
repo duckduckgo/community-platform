da4993e2516966ba577534ca9d68c0c9681e8c96
Maria Grazia Alastra <maria@duckduckgo.com>
2014-10-24 11:36:28 -0400

    IA Pages - make the new page editable

--- js/ia_pages/IAPageEdit.js
+++ js/ia_pages/IAPageEdit.js
@@ -17,9 +17,15 @@
                 console.log("for ia id '%s'", DDH_iaid);
 
                 $.getJSON("/ia/view/" + DDH_iaid + "/json", function(x) {
-				    var ia = Handlebars.templates.edit_page(x);
+				    var ia = Handlebars.templates.editpage(x);
 				    $(".ia-single").html(ia);
                     $(".ia-single .dev-info").on('click', DDH.IAPageEdit.prototype.expand.bind(this));
+					$(".editable").on('click', function(evt) {
+						$(this).attr("contenteditable", "true");
+					}); 
+					$(".editable").on('focusout', function(evt) {
+						$(this).attr("contenteditable", "false");
+					});
                 });
             }
 	    },
@@ -27,7 +33,7 @@
 	    expand: function() {
 		    $(".ia-single .dev-info").addClass("hide");
 		    $(".ia-single .dev-info-details").removeClass("hide");
-		}        
+		}
     };
 
 })(DDH);
