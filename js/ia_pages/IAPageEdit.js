e636b21b8a4a59d21f9c68d57acdcacacadf84de
Maria Grazia Alastra <maria@duckduckgo.com>
2014-10-27 09:27:54 -0400

    IA Pages - keep save button disabled until some field is edited

--- js/ia_pages/IAPageEdit.js
+++ js/ia_pages/IAPageEdit.js
@@ -11,7 +11,8 @@
     // but for now the page is being built with xslate
     DDH.IAPageEdit.prototype = {
         init: function(ops) {
-            console.log("IAPageEdit.init()\n"); 
+            console.log("IAPageEdit.init()\n");
+            var edited = false; 
 
             if (DDH_iaid) {
                 console.log("for ia id '%s'", DDH_iaid);
@@ -20,44 +21,50 @@
                     var ia = Handlebars.templates.editpage(x);
                     $(".ia-single").html(ia);
                     $(".ia-single .dev-info").on('click', DDH.IAPageEdit.prototype.expand.bind(this));
+                    $(".editable").attr("contenteditable", "true");
                     $(".editable").on('click', function(evt) {
-                        $(this).attr("contenteditable", "true");
-                    }); 
-                    $(".editable").on('focusout', function(evt) {
-                        $(this).attr("contenteditable", "false");
+                        if ($("#save").hasClass("disabled")) {
+                            $("#save").removeClass("disabled");
+                        }
+
+                        if (!edited) {
+                            edited = true;
+                        }
                     });
 
                     $("#save").on('click', function(evt) {
-                        var topics = [];
-                        $(".ia_topic a.editable").each(function(index) {
-                            topics.push($(this).text());
-                        });
+                       if (edited) {
+                            var topics = [];
+                            $(".ia_topic a.editable").each(function(index) {
+                                topics.push($(this).text());
+                            });
 
-                        var examples = [];
-                        $("#examples a.other-examples").each(function(index) {
-                            examples.push($(this).text());
-                        });
+                            var examples = [];
+                            $("#examples a.other-examples").each(function(index) {
+                                examples.push($(this).text());
+                            });
                         
-                        var code = [];
-                        $("li.code.editable").each(function(index) {
-                            code.push($(this).text());
-                        });
+                            var code = [];
+                            $("li.code.editable").each(function(index) {
+                                code.push($(this).text());
+                            });
 
-                        var jqxhr = $.post("/ia/save", {
-                                          description : $("#desc").text(), 
-                                          name : $("#name").text(),
-                                          status : $(".status").text(),
-                                          topic : JSON.stringify(topics),
-                                          example : $("#examples a#primary").text(),
-                                          other_examples : JSON.stringify(examples),
-                                          code : JSON.stringify(code),
-                                          id : DDH_iaid
-                                    })
-                        .done(function(data) {
-                            if (data) {
-                                window.location = "/ia/view/" + DDH_iaid;
-                            }
-                        });
+                            var jqxhr = $.post("/ia/save", {
+                                            description : $("#desc").text(), 
+                                            name : $("#name").text(),
+                                            status : $(".status").text(),
+                                            topic : JSON.stringify(topics),
+                                            example : $("#examples a#primary").text(),
+                                            other_examples : JSON.stringify(examples),
+                                            code : JSON.stringify(code),
+                                              id : DDH_iaid
+                                        })
+                            .done(function(data) {
+                                if (data) {
+                                    window.location = "/ia/view/" + DDH_iaid;
+                                }
+                            });
+                        }
                     });
                 });
             }
