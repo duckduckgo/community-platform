8724b65228f2242835621bbe5041ff16890d2606
John Barrett <john@jbrt.org>
2014-11-02 11:24:31 +0000

    Table for noting failed logins per user with timestamp.

---
+++ lib/DDGC/DB/Result/User/FailedLogin.pm
@@ -0,0 +1,31 @@
+package DDGC::DB::Result::User::FailedLogin;
+# ABSTRACT: Result class for tracking login failures
+
+use Moose;
+use MooseX::NonMoose;
+extends 'DDGC::DB::Base::Result';
+use DBIx::Class::Candy;
+use namespace::autoclean;
+
+table 'user_failedlogin';
+
+column id => {
+	data_type => 'bigint',
+	is_auto_increment => 1,
+};
+primary_key 'id';
+
+column users_id => {
+	data_type => 'bigint',
+	is_nullable => 0,
+};
+
+column timestamp => {
+	data_type => 'timestamp with time zone',
+	set_on_create => 1,
+};
+
+belongs_to 'user', 'DDGC::DB::Result::User', 'users_id';
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
