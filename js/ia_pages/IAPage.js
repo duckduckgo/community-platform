c223bb0af9d75940d9705837a8f9aa5dcb8f4232
Maria Grazia Alastra <maria@duckduckgo.com>
2014-11-06 15:15:43 -0500

    IA Pages - use separate handlebars templates for each section in edit mode

--- js/ia_pages/IAPage.js
+++ js/ia_pages/IAPage.js
@@ -17,9 +17,10 @@
                 console.log("for ia id '%s'", DDH_iaid);
 
                 $.getJSON("/ia/view/" + DDH_iaid + "/json", function(x) {
+                    // Readonly mode templates
                     var name = Handlebars.templates.name(x);
                     var status = Handlebars.templates.status(x);
-                    var desc = Handlebars.templates.desc(x);
+                    var desc = Handlebars.templates.description(x);
                     var team = Handlebars.templates.team(x);
                     var topic = Handlebars.templates.topic(x);
                     var screens = Handlebars.templates.screens(x);
@@ -27,7 +28,32 @@
                     var related = Handlebars.templates.related(x);
                     var devinfo = Handlebars.templates.devinfo(x);
                     var issues = Handlebars.templates.issues(x);
-                    var ia_edit = Handlebars.templates.editpage(x);
+
+                    // Pre-Edit mode templates
+                    var pre_templates = {
+                        name : Handlebars.templates.pre_edit_name(x),
+                        status : Handlebars.templates.pre_edit_status(x),
+                        description : Handlebars.templates.pre_edit_description(x),
+                        team : Handlebars.templates.pre_edit_team(x),
+                        topic : Handlebars.templates.pre_edit_topic(x),
+                        examples : Handlebars.templates.pre_edit_examples(x),
+                        example_query : this.examples,
+                        other_queries : this.examples,
+                        related : Handlebars.templates.pre_edit_related(x),
+                        issues : Handlebars.templates.pre_edit_issues(x)
+                    };
+
+                    // Edit mode templates
+                    var edit_templates = {
+                        name : Handlebars.templates.edit_name(x),
+                        status : Handlebars.templates.edit_status(x),
+                        description : Handlebars.templates.edit_description(x),
+                        team : Handlebars.templates.edit_team(x),
+                        topic : Handlebars.templates.edit_topic(x),
+                        example_query : Handlebars.templates.edit_examples(x),
+                        other_queries : this.example_query,
+                        related : Handlebars.templates.edit_related(x),
+                    };
 
                     $(".ia-single").append(name);
                     $(".ia-single").append(status);
@@ -40,10 +66,20 @@
                     $(".ia-single").append(devinfo);
                     $(".ia-single").append(issues);
 
-                    $(".ia-single .dev-info").on('click', DDH.IAPage.prototype.expand.bind(this));
+                    $("body").on('click', '.ia-single .dev-info', DDH.IAPage.prototype.expand.bind(this));
 
                     $("#edit_activate").on('click', function(evt) {
-                        $(".ia-single").html(ia_edit);
+
+                        $(".ia-single").html(pre_templates.name);
+                        $(".ia-single").append(pre_templates.status);
+                        $(".ia-single").append(pre_templates.description);
+                        $(".ia-single").append(pre_templates.team);
+                        $(".ia-single").append(pre_templates.topic);
+                        $(".ia-single").append(screens);
+                        $(".ia-single").append(pre_templates.examples);
+                        $(".ia-single").append(pre_templates.related);
+                        $(".ia-single").append(devinfo);
+                        $(".ia-single").append(pre_templates.issues);
 
                         if (!($("#examples a.other-examples").length)) {
                             $("#primary").parent().find(".button.delete").addClass("hide");
@@ -51,6 +87,27 @@
 
                         $("#edit_disable").removeClass("hide");
                         $(this).hide();
+                    });
+
+                    $("body").on('mouseenter mouseleave', '.pre_edit', function(evt) {
+                        $(this).toggleClass("highlight");
+                    });
+
+                    $("body").on('click', '.pre_edit', function(evt) {
+                        var name = $(this).attr('name');
+                        var $obj;
+
+                        if (name === "example_query" || name === "other_queries") {
+                           $obj = $("#examples");
+                        } else if (name === "topic") {
+                            $obj = $("#topics");
+                        } else if (name === "code") {
+                            $obj = $(".dev-info-details");
+                        }else {
+                            $obj = $(this);
+                        }
+
+                        $obj.replaceWith(edit_templates[name]);
                     });
 
                     $("#edit_disable").on('click', function(evt) {
@@ -64,16 +121,19 @@
 
                     $("body").on('focusout keypress', '.editable input', function(evt) {
                         if (evt.type === 'focusout' || (evt.type === 'keypress' && evt.which === 13)) {
-                            if ($(this).parent().attr('id') === 'input_example') {
-                                var new_example = $(this).val();
-                                if (new_example !== '') {
-                                    $(this).parent().parent().before('<li class="editpage"><div class="button delete listbutton"><span>-</span></div>' +
-                                                   '<a class="editable other-examples newentry" name="other_queries" title="Try this example on DuckDuckGo" href="https://duckduckgo.com/?q=' +
-                                                    new_example + '"><input type="text" value="' + new_example + '" /></a></li>');
+                            var $obj = $(this).parent();
+                            var field = $obj.attr('name');
+                            var value = $(this).val();
+
+                            if ($obj.attr('id') === 'input_example') {
+                                if (value !== '') {
+                                    $obj.parent().before('<li class="editpage"><div class="button delete listbutton"><span>-</span></div>' +
+                                                   '<span class="editable other-examples newentry" name="other_queries" title="Try this example on DuckDuckGo">' +
+                                                   '<input type="text" value="' + value + '" /></span></li>');
                                 }
 
                                 $(this).val("");
-                                $(this).parent().addClass("hide");
+                                $obj.addClass("hide");
                                 $("#add_example").removeClass("hide");
 
                                 var $primary_button = $("#primary").parent().parent().find(".button.delete");
@@ -82,20 +142,18 @@
                                 }
                             }
 
-                            var field = $(this).parent().attr('name');
-                            var value;
-
-                            if (field !== "topic" && field !== "other_queries" && field !== "code") {
-                                value = $(this).val();
-                            } else {
+                            if (field === "topic" || field === "other_queries" || field === "code") {
                                 value = [];
                                 var selector;
                                 if (field === "topic") {
                                     selector = ".ia_topic a.editable input";
+                                    $obj = $("#topics");
                                 } else if (field === "other_queries") {
-                                    selector = "#examples a.other-examples input";
+                                    selector = "#examples .other-examples input";
+                                    $obj = $("#examples");
                                 } else {                            
                                     selector = "li.code.editable input";
+                                    $obj = $(".dev-info-details");
                                 }
 
                                 $(selector).each(function(index) {
@@ -107,7 +165,7 @@
                                 value = JSON.stringify(value);
                             }
 
-                            save(field, value, DDH_iaid);
+                            save(field, value, DDH_iaid, $obj);
 
                             if (evt.type === "keypress") {
                                 return false;
@@ -118,6 +176,7 @@
                     $("body").on("click", ".button.delete", function(evt) {
                         var field = $(this).parent().find("a.editable").attr('name');
                         var value;
+                        var $obj = $("#examples");
 
                         console.log("Field: " + field);
                         if (field === 'example_query') {
@@ -127,7 +186,7 @@
                                 $new_primary.parent().attr('name', 'example_query');
                                 $new_primary.parent().attr('id', 'primary');
                                 console.log("$new_primary: " + $new_primary.val());
-                                save(field, $new_primary.val(), DDH_iaid);
+                                save(field, $new_primary.val(), DDH_iaid, $obj);
                             }
 
                             field = 'other_queries';
@@ -146,10 +205,10 @@
                             }
                         });
 
-                        save(field, value, DDH_iaid);
+                        save(field, value, DDH_iaid, $obj);
                     });
 
-                    function save(field, value, id) {
+                    function save(field, value, id, $obj) {
                         var jqxhr = $.post("/ia/save", {
                             field : field, 
                             value : value,
@@ -160,6 +219,19 @@
                                 if ($("#error").hasClass("hide")) {
                                     $("#error").removeClass("hide");
                                 }
+                            } else {
+                                $.getJSON("/ia/view/" + DDH_iaid + "/json", function(new_x) {
+                                    var name;
+                                    if (field === "example_query" || field === "other_queries") {
+                                        name = "examples"
+                                    } else {
+                                        name = field;
+                                    }
+
+                                    pre_templates[field] = Handlebars.templates['pre_edit_' + name](new_x);
+                                    edit_templates[field] = Handlebars.templates['edit_' + name](new_x);
+                                    $obj.replaceWith(pre_templates[field]);
+                                });
                             }
                         });
                     }
