228ec890952e271c5a3c2fb5333767d51ba2392a
John Barrett <john@jbrt.org>
2014-11-09 11:16:17 +0000

    Adding a validation message to the forgotpw page - @ in username
    People currently supplying an email address will get a crash error.

--- lib/DDGC/Web/Controller/My.pm
+++ lib/DDGC/Web/Controller/My.pm
@@ -8,6 +8,7 @@ use DDGC::Config;
 use Time::HiRes qw/ sleep /;
 use Email::Valid;
 use Digest::MD5 qw( md5_hex );
+use Try::Tiny;
 
 BEGIN {extends 'Catalyst::Controller'; }
 
@@ -453,6 +454,12 @@ sub forgotpw :Chained('logged_out') :Args(0) {
 	}
 
 	$c->stash->{forgotpw_username} = lc($c->req->params->{ $c->session->{username_field} });
+
+	if ($c->stash->{forgotpw_username} =~ /@/) {
+		$c->stash->{username_at} = 1;
+		return $c->detach;
+	}
+
 	$c->session->{username_field} = $c->d->uid;
 	
 	my $user = $c->d->find_user($c->stash->{forgotpw_username});
