273b9787a69e17870390500f2cab10d100a5bf91
Maria Grazia Alastra <maria@duckduckgo.com>
2014-11-13 10:02:28 -0500

    ddgc_create_ia.pm - initialize all fields to avoid malformed JSON

--- script/ddgc_create_ia.pm
+++ script/ddgc_create_ia.pm
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-
+use utf8;
 use FindBin;
 use lib $FindBin::Dir . "/../lib";
 use strict;
@@ -18,11 +18,18 @@ my $ia = $d->rs('InstantAnswer')->find({lc id => $ia_id});
 
 die "id ".$ia_id." is already in use" if $ia;
 
-my $new_ia = $d->rs('InstantAnswer')->new({
+my $new_ia = $d->rs('InstantAnswer')->create({
         lc id => $ia_id,
         name => $ia_name,
-        status => $ia_status 
+        status => $ia_status,
+        dev_milestone => $ia_status,
+        description => '',
+        repo => '',
+        perl_module => '',
+        topic => [],
+        attribution => [],
+        example_query => '',
+        src_name => '' 
     });
-$new_ia->insert;
 
 print "new Instant Answer ".$ia_name." successfully created\n";
