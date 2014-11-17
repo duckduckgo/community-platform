4be9d0552f2c02210abf48e1886527dc04beaf74
John Barrett <john@jbrt.org>
2014-11-17 15:03:38 +0000

    Fix script which migrates filenames away from username paths.
    Back when, image uploads were not validated... Now they are alongside
    stripping them of any potentially identifying metadata.
    
    This change stops Imagemagick barfing on old, non-image uploads. It
    lists the filename to give the opportunity to validate (and delete) them.

--- script/ddgc_migrate_screenshot_names.pl
+++ script/ddgc_migrate_screenshot_names.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 
+use Try::Tiny;
 use FindBin;
 use lib $FindBin::Dir . "/../lib";
 use DDGC;
@@ -16,7 +17,18 @@ while (my $screenshot = $screenshots->next) {
     my $filename = $ddgc->config->mediadir . '/' . $media->filename;
     my $thumbnail = $ddgc->config->mediadir . '/thumbnail/' . $media->filename;
     ( -f $filename ) || next;
-    my $newmedia = $ddgc->rs('Media')->create_via_file($user, $filename) || next;
+    my $newmedia;
+    my $err = 0;
+    try {
+        $newmedia = $ddgc->rs('Media')->create_via_file($user, $filename) || next;
+    }
+    catch {
+        $err = 1;
+    };
+    if ($err) {
+        print STDERR "Skipping $filename\n";
+        next;
+    }
     $newmedia->upload_filename($media->upload_filename);
     $newmedia->update;
     $screenshot->media_id($newmedia->id);
