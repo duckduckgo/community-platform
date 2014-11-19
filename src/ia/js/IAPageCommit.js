06e9227fc0cabf0f39c8c1d2bda43f712aa381e3
Maria Grazia Alastra <maria@duckduckgo.com>
2014-11-19 16:05:44 -0500

    IA Pages - style click and hover events on the commit page

--- src/ia/js/IAPageCommit.js
+++ src/ia/js/IAPageCommit.js
@@ -19,6 +19,18 @@
                 $.getJSON("/ia/commit/" + DDH_iaid + "/json", function(x) {
                     var iapc = Handlebars.templates.commit_page(x);
                     $("#ia_commit").html(iapc);
+
+                    $("body").on('click', '.updates_list ul li', function(evt) {
+                        $(this).parent().find('li').removeClass("item_selected");
+                        $(this).removeClass("item_focused");
+                        $(this).addClass("item_selected");
+                    });
+
+                    $("body").on('mouseenter mouseleave', '.updates_list ul li', function(evt) {
+                        if (!$(this).hasClass("item_selected")) {
+                            $(this).toggleClass("item_focused");
+                        }
+                    });
                 });
             }
         }
