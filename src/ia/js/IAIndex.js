79acda8b3fe5903d47b117d41411c853657eb771
Jag Talon <jagtalon@duckduckgo.com>
2014-12-05 10:37:32 -0500

    IAIndex.js: Can now display data on the page.

--- src/ia/js/IAIndex.js
+++ src/ia/js/IAIndex.js
@@ -1,29 +1,13 @@
-(function() {
-    var availableTemplates = {
-        index: {
-            view: function(result) {
-
-            },
-            data: function(result) {
-
-                return this;
-            }
-        },
-        template: {
-            view: function(result) {
-
-            },
-            data: function(result) {
-
-                return this;
-            }
-        }
-    };
-
+$(function() {
+    // This class is responsible for abstracting away:
+    // - Choosing the right template depending on the URL.
+    // - Getting the data needed by the template.
+    // - Calling the right methods to manipulate and display the data.
     DDH.Index = {
-        init: function(field, value) {
+        init: function(field, value, availableTemplates) {
             this.field = field;
             this.value = value;
+            this.template = availableTemplates.index;
 
             if(this.field && this.value) {
                 this.url += "/" + field + "/" + value;
@@ -35,21 +19,44 @@
             return this;
         },
 
-        view: function() {
+        view: function(id) {
             var template = this.template;
             $.getJSON(this.url, function(result) {
-                template.data(result).view();
+                var html = template.data(result).view();
+                $(id).html(html);
             });
         },
         
-        url: "/ia/json",
-        template: availableTemplates.index
+        url: "/ia/json"
     };
 
-    $(function() {
-        var field = $("#ia_index").attr("field");
-        var value = $("#ia_index").attr("value");
+    // This hash lists implements the `view` and `data` methods of each template.
+    // - `data` is responsible for manipulating the data (filtering, mapping, etc.)
+    // - `view` is responsible for displaying the data on the page.
+    var availableTemplates = {
+        index: {
+            view: function() {
+                return Handlebars.templates.index({ia: this.result});
+            },
+            data: function(result) {
+                this.result = ;
+                return this;
+            }
+        },
+        template: {
+            view: function() {
 
-        DDH.Index.init(field, value).view();
-    });
-})();
+            },
+            data: function(result) {
+
+                return this;
+            }
+        }
+    };
+
+    // Initialize the class.
+    var id = "#ia_index";
+    var field = $(id).attr("field");
+    var value = $(id).attr("value");
+    DDH.Index.init(field, value, availableTemplates).view(id);
+});
