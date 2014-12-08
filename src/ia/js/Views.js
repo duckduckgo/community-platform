21aac022e762e11c667450a857498a1c2162d604
Jag Talon <jagtalon@duckduckgo.com>
2014-12-08 14:38:29 -0500

    Views.js: Move functions to the bottom.

--- src/ia/js/Views.js
+++ src/ia/js/Views.js
@@ -1,4 +1,33 @@
 (function(env) {
+    // This object implements the `view` and `data` methods of each view.
+    // - `data` is responsible for manipulating the data (filtering, mapping, etc.)
+    // - `view` is responsible for displaying the data on the page and adding DOM manip.
+    env.Views = {
+        // Index View: This is the default view. It lists all the IAs in a table.
+        index: {
+            view: function() {
+                append_html("index_view", this.result);
+                return this;
+            },
+            data: function(result) {
+                this.result = sort("name", result);
+                return this;
+            }
+        },
+        
+        // Template View: Used to show the screenshots of the IAs on the page.
+        template: {
+            view: function() {
+                append_html("template_view", this.result);
+                return this;
+            },
+            data: function(result) {
+                this.result = sort("name", result);
+                return this;
+            }
+        }
+    }
+
     function sort(what, result) {
         return result.sort(function(a, b) {
             if(a[what] > b[what]) {
@@ -14,32 +43,5 @@
     function append_html(template, result) {
         var html = Handlebars.templates[template]({ia: result});                
         $("#ia_index").html(html);
-    }
-
-    // This object implements the `view` and `data` methods of each view.
-    // - `data` is responsible for manipulating the data (filtering, mapping, etc.)
-    // - `view` is responsible for displaying the data on the page and adding DOM manip.
-    env.Views = {
-        // Index View: This is the default view. It lists all the IAs in a table.
-        index: {
-            view: function() {
-                append_html("index_view", this.result);
-            },
-            data: function(result) {
-                this.result = sort("name", result);
-                return this;
-            }
-        },
-        
-        // Template View: Used to show the screenshots of the IAs on the page.
-        template: {
-            view: function() {
-                append_html("template_view", this.result);
-            },
-            data: function(result) {
-                this.result = sort("name", result);
-                return this;
-            }
-        }
     }
 }(this));
