4457127d1622b6f583be1efa49a53fb531c68dc0
Jag Talon <jagtalon@duckduckgo.com>
2014-12-08 14:05:09 -0500

    Views.js: Abstract away appending HTML

--- src/ia/js/Views.js
+++ src/ia/js/Views.js
@@ -1,6 +1,6 @@
 (function(env) {
-    function sort(what, list) {
-        return list.sort(function(a, b) {
+    function sort(what, result) {
+        return result.sort(function(a, b) {
             if(a[what] > b[what]) {
                 return 1;
             } else if(a[what] < b[what]) {
@@ -11,18 +11,22 @@
         });
     }
 
+    function append_html(template, result) {
+        var html = Handlebars.templates[template]({ia: result});                
+        $("#ia_index").html(html);
+    }
+
     // This object implements the `view` and `data` methods of each view.
     // - `data` is responsible for manipulating the data (filtering, mapping, etc.)
-    // - `view` is responsible for displaying the data on the page.
+    // - `view` is responsible for displaying the data on the page and adding DOM manip.
     env.Views = {
         // Index View: This is the default view. It lists all the IAs in a table.
         index: {
             view: function() {
-                var html = Handlebars.templates.index_view({ia: this.result});                
-                $("#ia_index").html(html);
+                append_html("index_view", this.result);
             },
             data: function(result) {
-                this.result = sort('name', result);
+                this.result = sort("name", result);
                 return this;
             }
         },
@@ -30,11 +34,10 @@
         // Template View: Used to show the screenshots of the IAs on the page.
         template: {
             view: function() {
-                var html = Handlebars.templates.template_view({ia: this.result});
-                $("#ia_index").html(html);
+                append_html("template_view", this.result);
             },
             data: function(result) {
-                this.result = sort('name', result);
+                this.result = sort("name", result);
                 return this;
             }
         }
