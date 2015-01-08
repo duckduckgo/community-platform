6074a997f7ffdcd93443ee924422c461e9e7b58d
Jag Talon <jagtalon@duckduckgo.com>
2015-01-08 14:33:33 -0500

    IAIndex: Add back ia_list

--- src/ia/js/IAIndex.js
+++ src/ia/js/IAIndex.js
@@ -42,6 +42,7 @@
 
             $.getJSON(url, function(x) { 
                 $("#ia_index_header h2").text(x.length + " Instant Answers");
+                ind.ia_list = x;
                 ind.sort('name');
                 $list_item = $("#ia-list .ia-item");
                 $clear_filters = $("#clear_filters");
