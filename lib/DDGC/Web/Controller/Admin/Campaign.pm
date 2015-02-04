3429112450f480201408ad9f1ef4bb6de01c28b9
John Barrett <john@jbrt.org>
2015-02-04 15:40:57 +0000

    Updating bad response admin form.
    - Use first available campaign when not specified.
    - Present a textarea for usernames when username not specified.

--- lib/DDGC/Web/Controller/Admin/Campaign.pm
+++ lib/DDGC/Web/Controller/Admin/Campaign.pm
@@ -4,8 +4,7 @@ package DDGC::Web::Controller::Admin::Campaign;
 
 use Moose;
 BEGIN { extends 'Catalyst::Controller'; }
-use List::MoreUtils qw/ each_array /;
-use Try::Tiny;
+use List::MoreUtils qw/ uniq /;
 use namespace::autoclean;
 
 sub base : Chained('/admin/base') : PathPart('campaign') : CaptureArgs(0) {
@@ -21,31 +20,44 @@ sub index : Chained('base') : PathPart('') : Args(0) {
 sub bad_user_response : Chained('base') : PathPart('bad_user_response') : Args(0) {
     my ( $self, $c ) = @_;
     $c->add_bc('Bad response');
-    ($c->stash->{user}, $c->stash->{campaign}) = ($c->req->params->{user}, $c->req->params->{campaign});
+    $c->stash->{user} = $c->req->params->{user};
+    $c->stash->{multi_user} = $c->req->params->{multi_user};
+    $c->stash->{campaign} = $c->req->params->{campaign} // $c->d->config->first_active_campaign;
 
-    $c->stash->{campaign_options} = [ map { {
-            value => $_,
-            text  => $c->d->config->campaigns->{$_}->{id} . ' - ' . $c->d->config->campaigns->{$_}->{name},
-        } } keys $c->d->config->campaigns ];
-
+    if (!$c->stash->{user}) {
+        $c->stash->{no_user} = 1;
+    }
+    if (!$c->stash->{campaign}) {
+        $c->stash->{no_campaign} = 1;
+        return $c->detach;
+    }
     if ($c->req->params->{save_bad_response}) {
-        if (!$c->req->params->{user}) {
-            $c->stash->{user_not_supplied} = 1;
-            return $c->detach;
-        }
-        if (!$c->req->params->{campaign}) {
-            $c->stash->{campaign_not_selected} = 1;
-            return $c->detach;
-        }
-        my $user = $c->d->find_user($c->req->params->{user});
-        if (!$user) {
-            $c->stash->{user_not_found} = 1;
-            return $c->detach;
+        my @usernames = uniq grep { $_ } split /[^a-zA-Z0-9_\.]+/, $c->req->params->{multi_user};
+        ($c->req->params->{user}) && push @usernames, $c->req->params->{user};
+
+        for my $username (@usernames) {
+            my $user = $c->d->find_user($username);
+            if (!$user) {
+                push @{ $c->stash->{ users_not_found } }, $username;
+            }
+            else {
+                if ($user->responded_campaign( $c->stash->{campaign} )) {
+                    $user->set_bad_response($c->stash->{campaign});
+                }
+                else {
+                    push @{ $c->stash->{ users_not_responded } }, $username;
+                }
+            }
         }
 
-        if ($user->set_bad_response($c->stash->{campaign})) {
+        if (!$c->stash->{ users_not_found } && !$c->stash->{ users_not_responded }) {
             $c->stash->{bad_response_success} = 1;
         }
+        $c->stash->{ multi_user } = join ', ',
+            (( $c->stash->{ users_not_responded } ) ?
+                @{ $c->stash->{ users_not_responded } } : (),
+            ( $c->stash->{ users_not_found } ) ?
+                @{ $c->stash->{ users_not_found } } : ())
     }
 }
 
