18dcd9073c4ce417dbaec70b2930e6f5586f3706
Maria Grazia Alastra <maria@duckduckgo.com>
2015-03-05 15:04:51 -0500

    Dev Pipeline - live view - add filtering functionality based on GH issues tags

--- src/ia/js/IADevPipeline.js
+++ src/ia/js/IADevPipeline.js
@@ -25,6 +25,25 @@
                 $("#dev_pipeline").html(iadp);
             });
 
+            $("body").on('click', ".filter-issues__item__checkbox", function(evt) {
+                if ($(this).hasClass("icon-check-empty")) {
+                    $(".icon-check").removeClass("icon-check").addClass("icon-check-empty");
+
+                    $(this).removeClass("icon-check-empty");
+                    $(this).addClass("icon-check");
+
+                    var issue_tag = $(this).attr("id");
+
+                    $("#pipeline-live__list .pipeline-live__list__item").hide();
+                    $("#pipeline-live__list .pipeline-live__list__item." + issue_tag).show();
+                } else {
+                    $(this).removeClass("icon-check");
+                    $(this).addClass("icon-check-empty");
+
+                    $("#pipeline-live__list .pipeline-live__list__item").show();
+                }
+            });
+
             $("#filter-team_checkbox").click(function(evt) {
                 $(this).toggleClass("icon-check");
                 $(this).toggleClass("icon-check-empty");
