60833d131d02c4b1562aac7c48ca1a5bc767e55b
jdorweiler <jddorweiler@gmail.com>
2015-04-03 10:04:09 -0400

    fix sql again

--- sql/1.027/updates_table.sql
+++ sql/1.027/updates_table.sql
@@ -1,12 +1,12 @@
 BEGIN;
 CREATE TABLE "instant_answer_updates" (
-      "instant_answer_id" text NOT NULL,
-      "field" text NOT NULL,
-      "value" text NOT NULL,
-      "timestamp" text NOT NULL,
-      PRIMARY KEY ("id")
+  "instant_answer_id" text NOT NULL,
+  "field" text NOT NULL,
+  "value" text NOT NULL,
+  "timestamp" text NOT NULL
 );
 
 ALTER TABLE "instant_answer_updates" ADD CONSTRAINT "instant_answer_updates_fk_instant_answer_id" FOREIGN KEY ("instant_answer_id")
   REFERENCES "instant_answer" ("id") ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE;
+
 COMMIT;
