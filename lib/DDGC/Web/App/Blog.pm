29eb6a5dd17e4cadf8b45357456ed6fa3bda97d5
John Barrett <john@jbrt.org>
2015-04-08 13:54:18 -0400

    Blog app renders base template

--- lib/DDGC/Web/App/Blog.pm
+++ lib/DDGC/Web/App/Blog.pm
@@ -6,21 +6,8 @@ use DDGC::Web::Plugin::Config;
 use DDGC::Web::Plugin::Session;
 
 get '/' => sub {
-    my $user = schema->resultset('User')->find({ username => session('__user') });
-    return 'None' if !$user;
-    my @templates = ( qw/base.tx loggedinuser.tx/ );
-    $xslate->{functions}->{next_template} = sub {
-        shift @templates;
-    };
-    $xslate->{functions}->{u} = sub {
-        '/';
-    };
-    return $xslate->render(shift @templates, {
-        user => $user,
-        c => { d => $d, user => $user },
-        next_template => sub { shift @templates; },
-        u => sub { $c->chained_uri(map { (ref $_ eq 'ARRAY') ? @{$_} : $_ } @_) },
-    });
+    request->var( templates => ['blog'] );
+    template('base', request->vars);
 };
 
 1;
