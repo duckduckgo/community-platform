b8e3fce438d13c1a8699aa5c0cfcd6975ab08887
John Barrett <john@jbrt.org>
2015-04-08 07:48:17 -0400

    Plugin inserts a wildcard route into an application to get current user
    - Regex /.*/ route created for each supported method to catch all requests
    - Coderef pulls DB user (or undef) from session vars (or lack thereof)
    - User result instance is stored in request variable 'user'
    - Request is then passed to next matching route
    
    `use DDGC::Web::Plugin::Session;` if you want the current user.

---
+++ lib/DDGC/Web/Plugin/Session.pm
@@ -0,0 +1,38 @@
+package DDGC::Web::Plugin::Session;
+
+use Dancer2;
+use Dancer2::Plugin;
+use DDGC::Config;
+
+on_plugin_import {
+    my ( $dsl ) = @_;
+    die "No schema method in app. Did you load DBIC::Plugin::DBIC before DDGC::Web::Plugin::Session?" if !( $dsl->can('schema'));
+
+    my $schema = $dsl->schema;
+    my $settings = plugin_setting();
+
+    for (qw/delete get head options patch post put/) {
+
+        $dsl->app->add_route(
+            method => $_,
+            regexp  => qr/.*/,
+
+            code    => sub {
+                my ($app) = @_;
+                $dsl->request->var( user =>
+                    $schema->resultset('User')->find(
+                        { username => $dsl->session('__user') }
+                    )
+                );
+                pass;
+            },
+
+        );
+
+    }
+
+};
+
+register_plugin for_versions => [2];
+
+1;
