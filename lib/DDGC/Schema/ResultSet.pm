930853f3defa3aa36f2403185b65bf499d5a2ba2
John Barrett <john@jbrt.org>
2015-04-16 06:00:25 -0400

    Turns out you don't really want all the Helper classes loaded.
    Lots of invisible automagic.

--- lib/DDGC/Schema/ResultSet.pm
+++ lib/DDGC/Schema/ResultSet.pm
@@ -3,6 +3,6 @@ package DDGC::Schema::ResultSet;
 use Moo;
 extends 'DBIx::Class::ResultSet';
 
-__PACKAGE__->load_components('Helper::ResultSet');
+__PACKAGE__->load_components('Helper::ResultSet::Me');
 
 1;
