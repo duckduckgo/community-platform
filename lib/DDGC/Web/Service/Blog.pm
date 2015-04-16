cce7f4eabe4621456e5e4788df42880e75ad2990
John Barrett <john@jbrt.org>
2015-04-16 10:12:51 -0400

    Extract blog rs to sub and add some route handlers for pages / page sizes

--- lib/DDGC/Web/Service/Blog.pm
+++ lib/DDGC/Web/Service/Blog.pm
@@ -4,12 +4,31 @@ use DDGC::Base::Web::Service;
 
 sub pagesize { 20 }
 
-get '/' => sub {
-    my $posts = rset('User::Blog')->company_blog->search({}, {
-        page => param('page') || 1,
-        rows => pagesize,
+sub posts_page {
+    my ( $page, $pagesize ) = @_;
+    ($page) && ( $page = int( abs( $page + 0 ) ) );
+    $page ||= 1;
+    ($pagesize) && ( $pagesize = int( abs( $pagesize + 0 ) ) );
+    $pagesize ||= pagesize;
+    ($pagesize > pagesize) && ($pagesize = pagesize);
+
+    rset('User::Blog')->company_blog->search_rs({}, {
+        order_by => { -desc => 'id' },
+        rows     => $pagesize,
+        page     => $page,
     });
-    { posts => $posts };
+}
+
+get '/' => sub {
+    { posts => posts_page( param_hmv('page'), param_hmv('pagesize') ) };
+};
+
+get '/page/:page' => sub {
+    { posts => posts_page( params('route')->{page} ) };
+};
+
+get '/page/:page/pagesize/:pagesize' => sub {
+    { posts => posts_page( params('route')->{page}, params('route')->{pagesize} ) };
 };
 
 1;
