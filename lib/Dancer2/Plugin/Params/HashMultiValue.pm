869a42e24839e0fdf4dcb89cf9bba12d6b1f3ba0
John Barrett <john@jbrt.org>
2015-04-16 09:12:13 -0400

    Plugin to flatten mutiple same request variables into a Hash::MultiValue
    - Should use route vars explicitly to prevent this
    - Does not implement source (i.e. route, body, query)
    - This is "temporary" until the following issues are resolved:
      - https://github.com/PerlDancer/Dancer2/issues/726
      - https://github.com/PerlDancer/Dancer2/issues/739

---
+++ lib/Dancer2/Plugin/Params/HashMultiValue.pm
@@ -0,0 +1,29 @@
+package Dancer2::Plugin::Params::HashMultiValue;
+
+use strict;
+use warnings;
+
+use Hash::MultiValue;
+use Dancer2::Plugin;
+
+register params_hmv => sub {
+    my ( $dsl ) = @_;
+    my $request = $dsl->app->request;
+
+    if ( !$request->var('plugin_params_hmv') ) {
+       $request->var('plugin_params_hmv' =>
+            Hash::MultiValue->from_mixed( $dsl->params )
+        );
+   }
+
+    return $request->var('plugin_params_hmv');
+};
+
+register param_hmv => sub {
+    my ( $dsl, $param ) = @_;
+    $dsl->params_hmv->{$param};
+};
+
+register_plugin;
+
+1;
