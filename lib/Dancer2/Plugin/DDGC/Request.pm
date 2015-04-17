3462595ee0e47cf601112da8dd845e51119da4a0
John Barrett <john@jbrt.org>
2015-04-17 09:40:48 -0400

    Have DDGC::Request forward the session cookie.
    plack_session is pretty cheap to forward by just adding its id to the header.
    
    This means the current user context will be passed along in requests
    happening in the back end, i.e. not initiated directly by the session owner's
    user agent.

--- lib/Dancer2/Plugin/DDGC/Request.pm
+++ lib/Dancer2/Plugin/DDGC/Request.pm
@@ -18,6 +18,14 @@ sub _uri_for {
         : $dsl->uri_for( $route, $params );
 }
 
+sub _apply_session_to_req {
+    my ( $dsl, $req ) = @_;
+    if ( $dsl->can('session') && $dsl->session->id ) {
+        $req->header(
+            Cookie => 'plack_session=' . $dsl->session->id,
+        );
+    }
+}
 on_plugin_import {
     my ( $dsl ) = @_;
     die "Need to load Dancer2::Plugin::RootURIFor before Dancer2::Plugin::DDGC::Request" if !($dsl->can('root_uri_for'));
@@ -25,9 +33,15 @@ on_plugin_import {
 
 register ddgcr_get => sub {
     my ( $dsl, $route, $params ) = @_;
-    my $res = $ua->get( _uri_for( $dsl, $route, $params ) );
+
+    my $req = HTTP::Request->new(
+        GET => _uri_for( $dsl, $route, $params )
+    );
+    _apply_session_to_req( $dsl, $req );
+
+    my $res = $ua->request( $req );
     $res->{ddgcr} = ( $res->is_success )
-        ? from_json( $res->decoded_content )
+        ? JSON::from_json( $res->decoded_content )
         : undef;
     return $res;
 };
@@ -36,9 +50,12 @@ register ddgcr_post => sub {
     my ( $dsl, $route, $data ) = @_;
 
     $data = to_json($data, { convert_blessed => 1 }) if ref $data;
-    my $req = HTTP::Request->new( POST => _uri_for( $dsl, $route ) );
+    my $req = HTTP::Request->new(
+        POST => _uri_for( $dsl, $route )
+    );
     $req->content_type( 'application/json' );
     $req->content( $data );
+    _apply_session_to_req( $dsl, $req );
 
     $ua->request( $req );
 };
