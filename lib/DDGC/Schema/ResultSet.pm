f9e0a378982dcbe0148047b9a03fb86b5512e637
John Barrett <john@jbrt.org>
2015-04-18 13:13:01 -0400

    Add a ->for_user clause to ResultSet class
    - Requires a Result::User
    - Populates current_user with the user instance for use in RS classes

--- lib/DDGC/Schema/ResultSet.pm
+++ lib/DDGC/Schema/ResultSet.pm
@@ -1,7 +1,21 @@
 package DDGC::Schema::ResultSet;
 
+use Carp;
 use Moo;
 extends 'DBIx::Class::ResultSet';
+
+has current_user => (
+    is  => 'rw',
+    isa => sub {
+        confess "Not a user" if ref $_[0] ne 'DDGC::Schema::Result::User';
+    },
+);
+
+sub for_user {
+    my ( $self, $user ) = @_;
+    $self->current_user($user) if ( $user );
+    return $self;
+}
 
 __PACKAGE__->load_components('Helper::ResultSet::Me');
 
