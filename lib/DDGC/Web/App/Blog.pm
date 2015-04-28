03ac019fc39f19a7fb36d1778200f6fc46a9513b
John Barrett <john@jbrt.org>
2015-04-28 08:49:18 -0400

    Route handlers for individual blog posts.
    - Now keyed on id with uri as a slug
    - Redirects if slug has been manipulated
    - Should handle old '/blog/$uri' links with redirect

--- lib/DDGC/Web/App/Blog.pm
+++ lib/DDGC/Web/App/Blog.pm
@@ -1,6 +1,7 @@
 package DDGC::Web::App::Blog;
 
 use DDGC::Base::Web::App;
+use Scalar::Util qw/ looks_like_number /;
 
 get '/' => sub {
     my $page = param_hmv('page') || 1;
@@ -16,6 +17,41 @@ get '/' => sub {
 
 get '/page/:page' => sub {
     forward '/', { params('route') };
-}
+};
+
+get '/:id/:uri' => sub {
+    my $params = params('route');
+    my $post;
+    my $res = ddgcr_get [ 'Blog', 'post' ], { id => $params->{id} };
+    if ( $res->is_success ) {
+        $post = $res->{ddgcr}->{post};
+
+        if ( $post->{uri} ne $params->{uri} ) {
+            redirect '/' . $post->{id} . '/' . $post->{uri};
+        }
+
+        template 'blog/index', { $res->{ddgcr} };
+    }
+    else {
+        status 404;
+    }
+};
+
+get '/:uri_or_id' => sub {
+    my $uri_or_id = params('route')->{uri_or_id};
+    my $req = ( looks_like_number( $uri_or_id ) )
+        ? ddgcr_get [ 'Blog', 'post' ], { id => int( $uri_or_id + 0 ) }
+        : ddgcr_get [ 'Blog', 'post', 'by_url' ], { url => $uri_or_id };
+    if ( $req->is_success ) {
+
+        my ($id, $uri) = (
+            $req->{ddgcr}->{post}->{id},
+            $req->{ddgcr}->{post}->{uri},
+        );
+
+        redirect "/$id/$uri";
+    };
+    status 404;
+};
 
 1;
