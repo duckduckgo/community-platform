440f4732382eac521e0c075ad797ed722a39f2fd
John Barrett <john@jbrt.org>
2015-04-30 08:24:55 -0400

    Adding abstract and docs for Dancer2::Plugin::DDGC::Validate

--- lib/Dancer2/Plugin/DDGC/Validate.pm
+++ lib/Dancer2/Plugin/DDGC/Validate.pm
@@ -1,5 +1,7 @@
 package Dancer2::Plugin::DDGC::Validate;
 
+# ABSTRACT: HTTP::Validate wrapper with pre-defined rulesets
+
 use Dancer2;
 use Dancer2::Plugin;
 use HTTP::Validate qw/ :validators /;
@@ -43,3 +45,49 @@ register validate => sub {
 register_plugin;
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Dancer2::Plugin::DDGC::Validate - HTTP::Validate wrapper with pre-defined rulesets
+
+=head1 SYNOPSIS
+
+    use Dancer2;
+    use Dancer2::Plugin::DDGC::Validate;
+
+    get '/post' => sub {
+        my $v = validate( '/blog.json', params );
+        if ( scalar $v->errors ) {
+            # handle errors, $v->errors is array of error messages
+        }
+        my $params = $v->values;
+    }
+
+=head1 DESCRIPTION
+
+Dancer2::Plugin::DDGC::Validate provides parameter validation via L<HTTP::Validate>.
+
+Rule sets are defined within this module, then called from request handlers.
+If you want new rule sets, add them to C<on_plugin_import> in this module.
+
+=head1 FUNCTIONS
+
+=head2 validate
+
+Expects the ruleset name and params hashref as arguments. Returns a
+L<HTTP::Validate::Result> instance.
+
+This instance can be checked for errors with the C<errors> accessor and filtered
+values are available in the C<values> accessor.
+
+    my $v = validate( '/blog.json/admin/post/new', params );
+    if ( scalar $v->errors ) {
+        bailout 400, [ $v->errors ]
+    }
+    rset('Blog')->create( $v->values )
+
+=cut
