fe5b9340acf6edb1405a2aa20b81f3d226d9949c
John Barrett <john@jbrt.org>
2015-05-01 08:29:07 -0400

    Attempting to add current_user context via rs accessor is misguided.
    The resultset instance should be a query plan.
    It should not hold or pass other state.

--- lib/DDGC/Schema/ResultSet.pm
+++ lib/DDGC/Schema/ResultSet.pm
@@ -6,24 +6,6 @@ use Carp;
 use Moo;
 extends 'DBIx::Class::ResultSet';
 
-has current_user => (
-    is  => 'rw',
-    isa => sub {
-        confess "Not a user" if ref $_[0] ne 'DDGC::Schema::Result::User';
-    },
-);
-
-sub for_user {
-    my ( $self, $user ) = @_;
-    $self->current_user($user) if ( $user );
-    return $self;
-}
-
-sub none {
-    my ( $self ) = @_;
-    return $self->search_rs(\['1=0']);
-}
-
 __PACKAGE__->load_components(qw/
     Helper::ResultSet::Me
     Helper::ResultSet::Shortcut::HRI
