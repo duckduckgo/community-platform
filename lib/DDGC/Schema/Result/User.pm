ba6be516fc4d1b54e856ed04bb9fbab721670cac
John Barrett <john@jbrt.org>
2015-05-14 11:50:52 -0400

    Avatar retrieval in user model

--- lib/DDGC/Schema/Result/User.pm
+++ lib/DDGC/Schema/Result/User.pm
@@ -5,6 +5,7 @@ package DDGC::Schema::Result::User;
 use Moo;
 extends 'DDGC::Schema::Result';
 use DBIx::Class::Candy;
+use Scalar::Util qw/ looks_like_number /;
 use namespace::autoclean;
 
 table 'users';
@@ -143,6 +144,35 @@ sub add_role {
     return 0 if grep { $_ eq $role } @{$self->flags};
     push @{$self->flags}, $role;
     $self->update;
+}
+
+sub username_filesystem_clean {
+    my ( $self ) = @_;
+    ( my $n = $self->username ) =~ s/[^A-Za-z0-9_-]+/_/g;
+    return $n;
+}
+
+sub avatar_path {
+    my ( $self, $size ) = @_;
+    my $username = $self->username_filesystem_clean;
+    my $fn = '/media/avatar/' . join '/', (
+        ( split '', $username )[0..1],
+        $username,
+    );
+    return $fn . (( $size )
+        ? "/${username}_${size}"
+        : '');
+}
+
+sub avatar {
+    my ( $self, $size ) = @_;
+    return '/static/images/profile.anonymous.png' if ( !$self->public );
+    $size ||= 32;
+    $size = 32 if ( !looks_like_number( $size ) );
+    my $avatar = $self->avatar_path( $size );
+    my $fullpath = join '/', ($self->app->config->{ddgc_config}->rootdir, $avatar);
+    return '/static/images/profile.male.png' if ( !-f $fullpath );
+    return $avatar;
 }
 
 sub TO_JSON {
