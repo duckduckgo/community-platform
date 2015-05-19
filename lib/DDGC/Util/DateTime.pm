5dc5e74d25cc8f2ce998c8c3860855f878a56671
John Barrett <john@jbrt.org>
2015-05-19 04:49:09 -0400

    lib/DDGC/Util/DateTime.pm formatting

--- lib/DDGC/Util/DateTime.pm
+++ lib/DDGC/Util/DateTime.pm
@@ -6,31 +6,31 @@ use warnings;
 use Exporter 'import';
 
 our @EXPORT = qw(
-	dur
-	dur_precise
+  dur
+  dur_precise
 );
 
 use DateTime;
 use DateTime::Format::Human::Duration;
 
 sub dur {
-	my ( $date ) = @_;
-	$date = DateTime->from_epoch( epoch => $date ) unless ref $date;
-	my $diff = DateTime->now - $date;
+    my ($date) = @_;
+    $date = DateTime->from_epoch( epoch => $date ) unless ref $date;
+    my $diff = DateTime->now - $date;
 
-	return DateTime::Format::Human::Duration->new->format_duration(
-		$diff,
-		units => [qw/ years months days hours minutes /],
-		significant_units => 2,
-		'past' => '%s ago',
-		'future' => 'in %s will be',
-		'no_time' => 'just now',
-	);
+    return DateTime::Format::Human::Duration->new->format_duration(
+        $diff,
+        units             => [qw/ years months days hours minutes /],
+        significant_units => 2,
+        past              => '%s ago',
+        future            => 'in %s will be',
+        no_time           => 'just now',
+    );
 }
 
 sub dur_precise {
-	my ( $date ) = @_;
-	dur( $date );
+    my ($date) = @_;
+    dur($date);
 }
 
 1;
