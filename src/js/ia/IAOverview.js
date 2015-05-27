f48b09670d191801449c9504caef48ccc4949146
Maria Grazia Alastra <maria@duckduckgo.com>
2015-05-27 14:29:56 -0400

    IA Pages Overview - add 'See All' button next to top GH issues

--- src/js/ia/IAOverview.js
+++ src/js/ia/IAOverview.js
@@ -7,16 +7,12 @@
     DDH.IAOverview.prototype = {
 
         init: function() {
-            console.log("IAOverview init()");
+            // console.log("IAOverview init()");
             var overview = this;
             var url = window.location.pathname.replace(/\/$/, '') + "/json";
             var username = $(".user-name").text();
 
-            console.log(url);
-
             $.getJSON(url, function(data) { 
-                console.log(data);
-
                 var template = Handlebars.templates.overview(data);
 
                 $("#ia-overview").html(template);
