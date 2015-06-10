852466596de21eaa466d3111fa3f6bae50fb5903
John Barrett <john@jbrt.org>
2015-06-10 05:39:01 -0400

    Activity Feed Result class

---
+++ lib/DDGC/Schema/Result/ActivityFeed.pm
@@ -0,0 +1,61 @@
+package DDGC::Schema::Result::ActivityFeed;
+# ABSTRACT: Activity feed reault class
+
+use Moo;
+extends 'DDGC::Schema::Result';
+use DBIx::Class::Candy;
+use DDGC::Util::Markup;
+
+table 'activity_feed';
+
+column id => {
+    data_type => 'bigint',
+    is_auto_increment => 1,
+};
+primary_key 'id';
+
+column created => {
+    data_type => 'timestamp with time zone',
+    set_on_create => 1,
+};
+
+column type => {
+    data_type => 'text',
+    is_nullable => 0,
+};
+
+column description => {
+    data_type => 'text',
+    is_nullable => 0,
+};
+
+column format => {
+    data_type => 'text',
+    default_value => 'markdown',
+};
+
+has renderer => (
+    is => 'ro',
+    lazy => 1,
+    builder => '_build_renderer',
+);
+sub _build_renderer {
+    DDGC::Util::Markup->new;
+}
+
+sub render_description{
+    my ( $self ) = @_;
+    my $format = $self->format || 'markdown';
+    $self->renderer->$format( $self->description );
+}
+
+sub TO_JSON {
+    my ( $self ) = @_;
+    +{
+        id          => $self->id,
+        type        => $self->type,
+        description => $self->render_description
+    }
+}
+
+1;
