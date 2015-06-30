493cf65c3a37e4a65534d30a96205cbf0d64039a
Eric Johnson <eric.git@iijo.org>
2015-06-30 09:45:19 -0400

    Added issues() and issue_comments()

--- lib/DDGC/GitHub/Cmds.pm
+++ lib/DDGC/GitHub/Cmds.pm
@@ -3,30 +3,50 @@ package DDGC::GitHub::Cmds;
 
 use Moose;
 use URI::Escape;
+use URI;
 
 with 'Net::GitHub::V3::Query';
 
 sub user {
-  my ( $self, $user ) = @_;
-  my $u = $user ? "/users/" . uri_escape($user) : '/user';
-  return $self->query($u);
+    my ( $self, $user ) = @_;
+    my $u = $user ? "/users/" . uri_escape($user) : '/user';
+    return $self->query($u);
 }
 
 sub list_user {
-  my ($self, $user, $type) = @_;
-  $user ||= $self->u;
-  $type ||= 'all';
-  my $u = "/users/" . uri_escape($user) . "/repos";
-  $u .= '?type=' . $type if $type ne 'all';
-  return $self->query($u);
+    my ($self, $user, $type) = @_;
+    $user ||= $self->u;
+    $type ||= 'all';
+    my $u = "/users/" . uri_escape($user) . "/repos";
+    $u .= '?type=' . $type if $type ne 'all';
+    return $self->query($u);
 }
 
 sub list_org {
-  my ($self, $org, $type) = @_;
-  $type ||= 'all';
-  my $u = "/orgs/" . uri_escape($org) . "/repos";
-  $u .= '?type=' . $type if $type ne 'all';
-  return $self->query($u);
+    my ($self, $org, $type) = @_;
+    $type ||= 'all';
+    my $u = "/orgs/" . uri_escape($org) . "/repos";
+    $u .= '?type=' . $type if $type ne 'all';
+    return $self->query($u);
+}
+
+sub issues {
+    my ($self, %args) = @_;
+    my $owner = $args{owner} || die "owner param required";
+    my $repo  = $args{repo}  || die "repo param required";
+    my $uri   = URI->new("/repos/$owner/$repo/issues");
+    $uri->query_form(%args);
+    return $self->query($uri->as_string);
+}
+
+sub issue_comments {
+    my ($self, %args) = @_;
+    my $owner  = $args{owner}  || die "owner param required";
+    my $repo   = $args{repo}   || die "repo param required";
+    my $number = $args{number} || die "number param required";
+    my $uri = URI->new("/repos/$owner/$repo/issues/$number/comments");
+    $uri->query_form(%args);
+    return $self->query($uri->as_string);
 }
 
 __build_methods(__PACKAGE__, (
@@ -35,8 +55,6 @@ __build_methods(__PACKAGE__, (
   commits_since   => { url => "/repos/%s/%s/commits?since=%s"   },
   pulls           => { url => "/repos/%s/%s/pulls?state=all"    },
   pulls_comments  => { url => "/repos/%s/%s/pulls/%s/comments"  },
-  issues          => { url => "/repos/%s/%s/issues"             },
-  issues_comments => { url => "/repos/%s/%s/issues/%s/comments" },
   issues_since    => { url => "/repos/%s/%s/issues?since=%s"    },
 ));
 
