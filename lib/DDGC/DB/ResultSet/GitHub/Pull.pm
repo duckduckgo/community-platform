41d57e1c3f06582bc9d1de694b7a31abd901dc49
Eric Johnson <eric.git@iijo.org>
2015-07-01 15:10:42 -0400

    added most_recent()

--- lib/DDGC/DB/ResultSet/GitHub/Pull.pm
+++ lib/DDGC/DB/ResultSet/GitHub/Pull.pm
@@ -5,10 +5,16 @@ use Moose;
 extends 'DDGC::DB::Base::ResultSet';
 use namespace::autoclean;
 
-#sub with_author_date {
-#    my ($self, $operator, $date) = @_;
-#	$self->search({ author_date => { $operator => $date } });
-#}
+sub most_recent {
+    my ($self) = @_;
+    return $self->search(
+        {},
+        {
+            rows => 1,
+            order_by => { -desc => 'updated_at' },
+        }
+    )->first;
+}
 
 no Moose;
 __PACKAGE__->meta->make_immutable( inline_constructor => 0 );
