020c0ec4d8e15b34f9a244fba0833165931b798c
Eric Johnson <eric.git@iijo.org>
2015-07-01 15:10:30 -0400

    Removed commented out code

--- lib/DDGC/DB/ResultSet/GitHub/Issue.pm
+++ lib/DDGC/DB/ResultSet/GitHub/Issue.pm
@@ -5,11 +5,6 @@ use Moose;
 extends 'DDGC::DB::Base::ResultSet';
 use namespace::autoclean;
 
-#sub with_author_date {
-#    my ($self, $operator, $date) = @_;
-#	$self->search({ author_date => { $operator => $date } });
-#}
-
 sub most_recent {
     my ($self) = @_;
     return $self->search(
