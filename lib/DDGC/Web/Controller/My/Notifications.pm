1a008fb38fcdb462cde39c52d550f48bc53c7a5e
John Barrett <john@jbrt.org>
2015-07-02 05:57:24 -0400

    Stop using xslate results call for following view

--- lib/DDGC/Web/Controller/My/Notifications.pm
+++ lib/DDGC/Web/Controller/My/Notifications.pm
@@ -58,11 +58,11 @@ sub following :Chained('base') :Args(0) {
 	my ( $self, $c ) = @_;
 	$c->add_bc('Following');
 	$c->pager_init($c->action,'30');
-	$c->stash->{user_notification_matrixes} = $c->user->search_related('user_notification_matrixes',{
+	$c->stash->{user_notification_matrixes} = [ $c->user->search_related('user_notification_matrixes',{
 		'me.context_id' => { '!=' => undef },
 	},{
 		order_by => { -desc => 'me.created' },
-	})->prefetch_all->paging($c->stash->{page},$c->stash->{pagesize});
+	})->prefetch_all->paging($c->stash->{page},$c->stash->{pagesize}) -> all ];
 }
 
 sub all_done :Chained('base') :Args(0) {
