582162f1b6bcd4ed8ac5dbedd9a32e451cf2b0ae
Eric Johnson <eric.git@iijo.org>
2015-07-02 19:00:36 -0400

    Minor formatting changes

--- lib/DDGC/DB/Result/GitHub/Comment.pm
+++ lib/DDGC/DB/Result/GitHub/Comment.pm
@@ -9,15 +9,15 @@ use namespace::autoclean;
 
 table 'github_comment';
 
-primary_column id          => {data_type => 'bigint', is_auto_increment => 1};
-unique_column  github_id   => {data_type => 'bigint', is_nullable => 0};
-column github_repo_id      => {data_type => 'bigint', is_nullable => 0};
-column github_user_id      => {data_type => 'bigint', is_nullable => 0};
-column number              => {data_type => 'bigint', is_nullable => 0};
-column body                => {data_type => 'text',   is_nullable => 0};
-column created_at          => {data_type => 'timestamp with time zone', is_nullable => 0};
-column updated_at          => {data_type => 'timestamp with time zone', is_nullable => 0};
-column gh_data             => {
+primary_column id        => {data_type => 'bigint', is_auto_increment => 1};
+unique_column  github_id => {data_type => 'bigint', is_nullable => 0};
+column github_repo_id    => {data_type => 'bigint', is_nullable => 0};
+column github_user_id    => {data_type => 'bigint', is_nullable => 0};
+column number            => {data_type => 'bigint', is_nullable => 0};
+column body              => {data_type => 'text',   is_nullable => 0};
+column created_at        => {data_type => 'timestamp with time zone', is_nullable => 0};
+column updated_at        => {data_type => 'timestamp with time zone', is_nullable => 0};
+column gh_data           => {
     data_type        => 'text',
     is_nullable      => 0,
     serializer_class   => 'JSON',
@@ -26,11 +26,11 @@ column gh_data             => {
 
 belongs_to github_repo => 'DDGC::DB::Result::GitHub::Repo',
     { 'foreign.id' => 'self.github_repo_id' },
-    { on_delete => 'cascade' };
+    { on_delete    => 'cascade' };
 
 belongs_to github_user => 'DDGC::DB::Result::GitHub::User',
     { 'foreign.id' => 'self.github_user_id' },
-    { on_delete => 'cascade' };
+    { on_delete    => 'cascade' };
 
 belongs_to github_issue => 'DDGC::DB::Result::GitHub::Issue',
     { 'foreign.number'         => 'self.number',
