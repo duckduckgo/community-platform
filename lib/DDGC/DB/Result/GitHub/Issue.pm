0734e95a9532c88b4f600d60b5105e411d1bc071
Eric Johnson <eric.git@iijo.org>
2015-07-10 17:20:45 -0400

    number column should have the same type in every table

--- lib/DDGC/DB/Result/GitHub/Issue.pm
+++ lib/DDGC/DB/Result/GitHub/Issue.pm
@@ -16,7 +16,7 @@ unique_column github_id => {data_type => 'bigint',  is_nullable => 0};
 column github_repo_id   => {data_type => 'bigint',  is_nullable => 0};
 column github_user_id   => {data_type => 'bigint',  is_nullable => 0};
 column idea_id          => {data_type => 'bigint',  is_nullable => 1};
-column number           => {data_type => 'int',     is_nullable => 0};
+column number           => {data_type => 'bigint',  is_nullable => 0};
 column comments         => {data_type => 'int',     is_nullable => 0};
 column title            => {data_type => 'text',    is_nullable => 0};
 column body             => {data_type => 'text',    is_nullable => 0};
