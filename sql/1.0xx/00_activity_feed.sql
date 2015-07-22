f56260fc8f0c88b6628e2c8d0dee9818242ffb4f
John Barrett <john@jbrt.org>
2015-07-22 10:41:03 -0400

    Update activity_feed DDL

--- sql/1.0xx/00_activity_feed.sql
+++ sql/1.0xx/00_activity_feed.sql
@@ -3,7 +3,11 @@ BEGIN;
 CREATE TABLE "activity_feed" (
   "id" serial NOT NULL,
   "created" timestamp with time zone NOT NULL,
-  "type" text NOT NULL,
+  "category" text NOT NULL,
+  "action" text,
+  "meta1" text,
+  "meta2" text,
+  "meta3" text,
   "description" text NOT NULL,
   "format" text DEFAULT 'markdown' NOT NULL,
   "for_user" bigint,
