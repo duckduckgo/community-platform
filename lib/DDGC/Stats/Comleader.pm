b6f7a6b665365f17de16f2999f6207116ed98afb
Eric Johnson <eric.git@iijo.org>
2015-07-23 14:34:40 -0400

    comleader contribution stats

---
+++ lib/DDGC/Stats/Comleader.pm
@@ -0,0 +1,76 @@
+package DDGC::Stats::Comleader;
+# ABSTRACT: Calculate stats of github users to find potential comleaders
+use Moo;
+use 5.16.0;
+
+use DDP;
+use Number::Format qw/round/;
+use DDGC::Stats::GitHub::Utils qw/duration_to_minutes human_duration/;
+use DateTime;
+use List::AllUtils qw/any/;
+
+use DDGC;
+
+=head1 SYNOPSIS
+
+    my %report = DDGC::Stats::Comleader->report();
+
+=cut
+
+has db => (is => 'lazy');
+
+sub _build_db { DDGC->new->db }
+
+sub report {
+    my ($class, %args) = @_;
+    my $self = $class->new(%args);
+
+    my $report = $self->support();
+
+    return $report;
+}
+
+sub support {
+    my ($self) = @_;
+
+    # TODO: exclude staff
+    my $rs = $self->db->resultset('Comment')
+        ->search_rs(
+            { 
+                'me.ghosted' => 0,
+                seen_live    => 1,
+                context      => [qw/DDGC::DB::Result::Idea DDGC::DB::Result::Thread/],
+            },
+            { 
+                select => [ { count => 'me.id', -as => 'comments' }, 'users_id' ],
+                group_by => 'users_id',
+            },
+        )
+#       ->prefetch('user')
+#       ->columns([qw/users_id/])
+#       ->group_by('users_id')
+        ->order_by({ -desc => 'comments' });
+
+    while (my $comment = $rs->next) {
+        say sprintf "username %-50s %-50s",
+            $comment->user->username,
+            $comment->user->email // '';
+    }
+
+}
+
+
+# query flags column for translation_manager
+
+# 50 live translations
+#sub translations {
+#    my ($self) = @_;
+#
+#    # live translations live in token_language
+#    # publicy viewable users (ignore hidden users -- see public flag)
+#    # not staff
+#    # ghosted 0
+#}
+
+
+1;
---
+++ script/ddgc_comleader_contributions.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use DDGC::Stats::Comleader;
+DDGC::Stats::Comleader->report();
