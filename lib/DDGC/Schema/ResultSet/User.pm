e3ee2f784fa5a1955f7ed4efc6d103f07c965440
John Barrett <john@jbrt.org>
2015-07-28 08:09:10 -0400

    User RS class, including method to retrieve unsent activity after a timestamp

---
+++ lib/DDGC/Schema/ResultSet/User.pm
@@ -0,0 +1,22 @@
+package DDGC::Schema::ResultSet::User;
+
+# ABSTRACT: Resultset class for users
+
+use Moo;
+extends 'DDGC::Schema::ResultSet';
+
+sub unsent_activity_from_date {
+    my ( $self, $datetime ) = @_;
+
+    if ( ref $datetime && $datetime->isa('DateTime') ) {
+        $datetime = $self->format_datetime( $datetime );
+    }
+
+    $self->search_rs({
+        'activity.created' => { '>=' => $datetime },
+    })->prefetch( { subscriptions => 'activity' } )
+      ->having( \[ 'count(activity.*) > 0' ] )
+      ->group_by( 'users' );
+}
+
+1;
