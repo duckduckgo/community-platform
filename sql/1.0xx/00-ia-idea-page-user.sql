35d6ac866e053645a8f921c26496e9e1d8332a96
John Barrett <john@jbrt.org>
2015-07-28 10:50:29 -0400

    DDL for IA Page, IA Idea, User relationships

---
+++ sql/1.0xx/00-ia-idea-page-user.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+ALTER TABLE idea ADD COLUMN claimed_by bigint;
+
+ALTER TABLE idea ADD COLUMN instant_answer_id text;
+
+ALTER TABLE idea ADD CONSTRAINT idea_fk_instant_answer_id FOREIGN KEY (instant_answer_id)
+  REFERENCES users (id) ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE;
+
+ALTER TABLE idea ADD CONSTRAINT idea_fk_claimed_by FOREIGN KEY (claimed_by)
+  REFERENCES users (id) ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE;
+
+COMMIT;
+
