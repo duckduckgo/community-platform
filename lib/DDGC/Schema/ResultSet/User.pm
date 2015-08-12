a16a0ff8bdfcb8cf9b9a33f30b55453e1c059d91
John Barrett <john@jbrt.org>
2015-08-12 10:39:11 -0400

    Optionally retrieve unsent activity up to a given timestamp

--- lib/DDGC/Schema/ResultSet/User.pm
+++ lib/DDGC/Schema/ResultSet/User.pm
@@ -3,17 +3,25 @@ package DDGC::Schema::ResultSet::User;
 # ABSTRACT: Resultset class for users
 
 use Moo;
+use DateTime;
 extends 'DDGC::Schema::ResultSet';
 
-sub unsent_activity_from_date {
-    my ( $self, $datetime ) = @_;
+sub unsent_activity_from_to_date {
+    my ( $self, $from, $to ) = @_;
 
-    if ( ref $datetime eq 'DateTime' ) {
-        $datetime = $self->format_datetime( $datetime );
+    $to //= DateTime->now;
+
+    if ( ref $to eq 'DateTime' ) {
+        $to = $self->format_datetime( $to );
+    }
+    if ( ref $from eq 'DateTime' ) {
+        $from = $self->format_datetime( $from );
     }
 
     $self->search_rs({
-        'activity.created' => { '>=' => $datetime },
+        'activity.created' => {
+            -between => [ $from, $to ],
+        },
     })->prefetch( { subscriptions => 'activity' } )
       ->having( \[ 'count(activity.*) > 0' ] )
       ->group_by( 'users' );
