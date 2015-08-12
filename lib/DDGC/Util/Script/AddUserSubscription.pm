491b4e1215d3e4483430033ee3156024a5d5daf0
John Barrett <john@jbrt.org>
2015-08-12 08:44:07 -0400

    Moo class to handle adding user subscriptions

---
+++ lib/DDGC/Util/Script/AddUserSubscription.pm
@@ -0,0 +1,66 @@
+use strict;
+use warnings;
+
+package DDGC::Util::Script::AddUserSubscription;
+
+use DDGC::Base::Web::Common;
+use Carp;
+use Moo;
+
+has subscriptions => (
+    is      => 'ro',
+    lazy    => 1,
+    builder => '_build_subscriptions',
+);
+
+sub _build_subscriptions {
+    config->{ddgc_config}->subscriptions;
+}
+
+has user => (
+    is      => 'ro',
+    lazy    => 1,
+    builder => '_build_user',
+    isa     => sub {
+        croak "Invalid user or user not found"
+          if ref $_[0] ne 'DDGC::Schema::Result::User';
+    }
+);
+
+sub _build_user {
+    my ( $self ) = @_;
+    rset('User')->find_by_username( $self->sub->{user} );
+}
+
+has sub => (
+    is  => 'ro',
+);
+
+sub add_user_subscription {
+    my ( $self ) = @_;
+    my $sub = $self->sub->{sub};
+
+    $self->user->find_or_create_related( 'subscriptions',
+         $self->subscriptions->$sub(
+             ( $self->sub->{meta1} )
+                ? ( meta1 => $self->sub->{meta1} )
+                : (),
+             ( $self->sub->{meta2} )
+                ? ( meta2 => $self->sub->{meta2} )
+                : (),
+             ( $self->sub->{meta3} )
+                ? ( meta3 => $self->sub->{meta3} )
+                : (),
+         )
+    );
+}
+
+sub execute {
+    my ( $self ) = @_;
+
+    croak sprintf( "Unknown subscription type %s", $self->sub->{sub} )
+      if !$self->subscriptions->can( $self->sub->{sub} );
+    return $self->add_user_subscription;
+}
+
+1;
