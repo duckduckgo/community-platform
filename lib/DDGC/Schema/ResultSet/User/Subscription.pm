447138a467592b9d302ee8580a195462e7e7fb3e
John Barrett <john@jbrt.org>
2015-08-18 04:32:05 -0400

    Subscription rs class, just to inherit role methods (like all_ref)

---
+++ lib/DDGC/Schema/ResultSet/User/Subscription.pm
@@ -0,0 +1,9 @@
+package DDGC::Schema::ResultSet::User::Subscription;
+
+# ABSTRACT: Resultset class for user subscriptions
+
+use Moo;
+extends 'DDGC::Schema::ResultSet';
+use List::MoreUtils qw( uniq );
+
+1;
