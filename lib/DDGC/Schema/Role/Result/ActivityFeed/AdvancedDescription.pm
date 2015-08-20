50e10b9d1557e748f3a1b752438b54d4582099b4
John Barrett <john@jbrt.org>
2015-08-20 11:10:51 -0400

    Role to describe activity in a more advanced way, if required / desired.

---
+++ lib/DDGC/Schema/Role/Result/ActivityFeed/AdvancedDescription.pm
@@ -0,0 +1,40 @@
+package DDGC::Schema::Role::Result::ActivityFeed::AdvancedDescription;
+use strict;
+use warnings;
+
+use Moo::Role;
+
+has xslate => (
+    is => 'ro',
+    lazy => 1,
+    builder => '_build_xslate',
+);
+sub _build_xslate {
+    Text::Xslate->new(
+        path => 'views',
+    );
+}
+
+sub describe {
+    my ( $self ) = @_;
+    my $sub = sprintf 'describe_%s',
+                join( '_', ( $self->category, $self->action ) );
+
+    return $self->$sub if $self->can( $sub );
+    return $self->render_description;
+}
+
+sub describe_instant_answer_updated {
+    my ( $self ) = @_;
+    my $updates = { map { split ',', $_ } ( $self->meta3 =~ /:(.*?):/g ) };
+    use DDP; p $updates;
+
+    $self->xslate->render(
+        'includes/activity_feed/instant_answer_updated.tx', {
+            activity => $self,
+            updates  => join( ', ', keys $updates ),
+        }
+    );
+}
+
+1;
