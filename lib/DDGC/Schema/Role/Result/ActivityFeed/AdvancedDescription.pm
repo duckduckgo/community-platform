c73eb7ac0ca76767a2b3abe0c689d9154ddf2a19
John Barrett <john@jbrt.org>
2015-08-21 11:30:14 -0400

    Fix fields filtering by limiting split to two strings.

--- lib/DDGC/Schema/Role/Result/ActivityFeed/AdvancedDescription.pm
+++ lib/DDGC/Schema/Role/Result/ActivityFeed/AdvancedDescription.pm
@@ -26,7 +26,7 @@ sub describe {
 
 sub describe_instant_answer_updated {
     my ( $self ) = @_;
-    my $updates = { map { split ',', $_ } ( $self->meta3 =~ /:(.*?):/g ) };
+    my $updates = { map { split ',', $_, 2 } ( $self->meta3 =~ /:(.*?):/g ) };
 
     $self->xslate->render(
         'includes/activity_feed/instant_answer_updated.tx', {
