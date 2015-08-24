5ea9a0df0f041b0efcae32e81bc7c2ce4e293669
John Barrett <john@jbrt.org>
2015-08-24 10:25:01 -0400

    Guard against generating mail if script is run accidentally

--- script/ddgc_activity_mailer.pl
+++ script/ddgc_activity_mailer.pl
@@ -7,5 +7,7 @@ use FindBin;
 use lib $FindBin::Dir . "/../lib";
 use DDGC::Util::Script::ActivityMailer;
 
+die 'This emails people... See crontab.' if (!$ARGV[0] || $ARGV[0] ne '--yes-really-send-email');
+
 DDGC::Util::Script::ActivityMailer->new->execute;
 
