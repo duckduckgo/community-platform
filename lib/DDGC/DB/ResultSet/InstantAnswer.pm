1a6f510ae5776de036fa5f0004648504f6cb34ff
John Barrett <john@jbrt.org>
2015-09-22 10:54:18 -0400

    Return Unix epoch date if we don't have a max updated for query

--- lib/DDGC/DB/ResultSet/InstantAnswer.pm
+++ lib/DDGC/DB/ResultSet/InstantAnswer.pm
@@ -4,17 +4,22 @@ package DDGC::DB::ResultSet::InstantAnswer;
 use Moose;
 extends 'DDGC::DB::Base::ResultSet';
 use namespace::autoclean;
+use DateTime;
 use DateTime::Format::Pg;
 
 sub last_modified {
     my ( $self ) = @_;
-    DateTime::Format::Pg->parse_datetime(
-        $self->search( {}, {
-            columns => [
-                { last_modified => { max => 'updated' } },
-            ],
-        } )->one_row->get_column('last_modified')
-    );
+    my $last_modified =  $self->search( {}, {
+        columns => [
+            { last_modified => { max => 'updated' } },
+        ],
+    } )->one_row->get_column('last_modified');
+
+    return DateTime::Format::Pg->parse_datetime(
+        $last_modified
+    ) if $last_modified;
+
+    return DateTime->new( year => 1970 );
 }
 
 1;
