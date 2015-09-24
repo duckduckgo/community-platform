020bd36a98bffd4b2a26174d5aae71d7e777252a
John Barrett <john@jbrt.org>
2015-09-24 13:45:40 -0400

    Fix return_if_not_modified calls

--- lib/DDGC/Web/Controller/InstantAnswer.pm
+++ lib/DDGC/Web/Controller/InstantAnswer.pm
@@ -7,7 +7,6 @@ use Time::Local;
 use JSON;
 use Net::GitHub::V3;
 use DateTime;
-use DateTime::Format::HTTP;
 
 my $INST = DDGC::Config->new->appdir_path."/root/static/js";
 
@@ -57,7 +56,7 @@ sub ialist_json :Chained('base') :PathPart('json') :Args() {
 
     my $rs = $c->d->rs('InstantAnswer');
 
-    $self->return_if_not_modified( $c, $rs->last_modified );
+    $c->return_if_not_modified( $rs->last_modified );
 
     my @ial = $rs->search(
         {'topic.name' => [{ '!=' => 'test' }, { '=' => undef}],
@@ -95,7 +94,7 @@ sub iarepo_json :Chained('iarepo') :PathPart('json') :Args(0) {
         {dev_milestone => 'complete'}]
     });
 
-    $self->return_if_not_modified( $c, $iarepo->last_modified );
+    $c->return_if_not_modified( $iarepo->last_modified );
 
     $iarepo = $iarepo->prefetch( { instant_answer_topics => 'topic' });
     my %iah;
@@ -577,7 +576,7 @@ sub ia_base :Chained('base') :PathPart('view') :CaptureArgs(1) {  # /ia/view/cal
 sub ia_json :Chained('ia_base') :PathPart('json') :Args(0) {
     my ( $self, $c) = @_;
 
-    $self->return_if_not_modified( $c, $c->stash->{ia}->updated );
+    $c->return_if_not_modified( $c->stash->{ia}->updated );
 
     my $ia = $c->stash->{ia};
     my $edited;
