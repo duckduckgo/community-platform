87cf5f06df20e7233b30723fa4f53328a47435ef
Zach Thompson <zach@duckduckgo.com>
2015-10-01 09:26:06 -0600

    Remove deletes; sql will fail if dupes are there and we can remove manually.  They *may* already be gone.

--- sql/1.084/update-release.sql
+++ sql/1.084/update-release.sql
@@ -1,13 +1,5 @@
 BEGIN:
 
-delete from instant_answer where id = 'crunchbase';
-delete from instant_answer where id = 'time_and_date_holiday';
-delete from instant_answer where id = '6169';
-delete from instant_answer where id = 'windows_shortcuts_cheat_sheet';
-delete from instant_answer where id = 'mlb_games';
-delete from instant_answer where id = '6303';
-delete from instant_answer where id = 'yoga_postures';
-
 alter table instant_answer add column release_version numeric(8,3);
 
 create unique index ia_unique_meta_id on instant_answer (meta_id);
