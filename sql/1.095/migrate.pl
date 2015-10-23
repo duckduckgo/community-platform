67e187e54ee492400753dfe0172538250e1516ba
Maria Grazia Alastra <maria@duckduckgo.com>
2015-10-23 08:08:46 -0400

    Add migration script for github usernames - thanks @jbarrett

---
+++ sql/1.095/migrate.pl
@@ -0,0 +1,7 @@
+for my $user ($ddgc->rs('User')->all) {
+    $user->update({ github_user => $user->data->{github} }) if  $user->data->{github};
+    if ( my $gh_user = $ddgc->rs('Github::UserLink')->search({ users_id => $user->id })->one_row ) {
+        $user->update({ github_user => $gh_user->data->{login} });
+    }
+}
+
