127a69a53e248c5d047534be0b3e7cc17c330c06
Maria Grazia Alastra <maria@duckduckgo.com>
2015-10-23 08:06:16 -0400

    github_user_link - remove unnecessary cols

--- lib/DDGC/DB/Result/GitHub/UserLink.pm
+++ lib/DDGC/DB/Result/GitHub/UserLink.pm
@@ -8,8 +8,6 @@ use namespace::autoclean;
 
 table 'github_user_link';
 
-primary_column id             => {data_type => 'bigint', is_auto_increment => 1};
-unique_column github_id       => {data_type => 'bigint', is_nullable       => 0};
-column data                   => {data_type => 'text',   is_nullable       => 0, serializer_class => 'JSON'};
-column users_id               => {data_type => 'bigint', is_nullable       => 0};
+primary column users_id       => {data_type => 'bigint', is_nullable => 0};
+column data                   => {data_type => 'text',   is_nullable => 0, serializer_class => 'JSON'};
 
