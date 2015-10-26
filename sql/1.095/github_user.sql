8b32cb4cacf7f72ccb566696f8f01e6948f61df5
Maria Grazia Alastra <maria@duckduckgo.com>
2015-10-26 08:06:01 -0400

    github_user.sql - table already exists

--- sql/1.095/github_user.sql
+++ sql/1.095/github_user.sql
@@ -1,8 +1,3 @@
 BEGIN;
-    CREATE TABLE github_user_link (
-        "users_id" bigint NOT NULL,
-        "data" text NOT NULL
-    );
-
     ALTER TABLE users ADD COLUMN "github_user" text;
 COMMIT;
