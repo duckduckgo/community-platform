6c712e6f4734f80e8f38da2ed0a2a6adadd1d07d
John Barrett <john@jbrt.org>
2015-11-05 07:28:26 -0500

    Move IA index fetch to rs class.

--- lib/DDGC/DB/ResultSet/InstantAnswer.pm
+++ lib/DDGC/DB/ResultSet/InstantAnswer.pm
@@ -5,4 +5,32 @@ use Moose;
 extends 'DDGC::DB::Base::ResultSet';
 use namespace::autoclean;
 
+sub ia_index_hri {
+    my ( $self, $limit, $last ) = @_;
+    my $ial = $self->search( {
+            'topic.name' => { '!=' => 'test' },
+            'me.dev_milestone' => { '=' => 'live'},
+        },
+        {
+            prefetch => { instant_answer_topics => 'topic' },
+            columns => [
+                qw/ name repo src_name
+                    dev_milestone description
+                    template id meta_id
+                /, ],
+            collapse => 1,
+        },
+    )->hri
+     ->order_by( 'me.name' );
+
+    $ial = $ial->rows( $limit ) if $limit;
+    $ial = $ial->search( {
+       'me.name' => { '>' => $last },
+    } ) if $last;
+
+    return $ial->all_ref;
+}
+
+
+
 1;
