41a44a7c4e3d0c0a23b65e452d6a138b488356e9
John Barrett <john@jbrt.org>
2015-11-06 11:59:11 -0500

    Removing RS/InstantAnswer/Blocks.pm - corresponding Result class is gone

--- lib/DDGC/DB/ResultSet/InstantAnswer/Blocks.pm
+++
@@ -1,9 +0,0 @@
-package DDGC::DB::ResultSet::InstantAnswer::Blocks;
-# ABSTRACT: Resultset class for Instant Answer Blocks
-
-use Moose;
-extends 'DDGC::DB::Base::ResultSet';
-use namespace::autoclean;
-
-1;
-
