74c6204381e3abfda05916f99d62fb3ba4601ac1
John Barrett <john@jbrt.org>
2015-12-16 08:21:07 -0500

    Re-linking github_user - DDL

--- sql/1.110/github_login.sql
+++ sql/1.110/github_login.sql
@@ -3,5 +3,13 @@ BEGIN;
 ALTER TABLE users RENAME COLUMN github_user TO github_user_plaintext;
 ALTER TABLE users ADD COLUMN github_access_token text;
 ALTER TABLE users ADD COLUMN github_user_linked text;
+ALTER TABLE users ADD COLUMN github_id bigint;
+ALTER TABLE users ADD CONSTRAINT users_github_id UNIQUE (github_id);
+
+ALTER TABLE github_user DROP CONSTRAINT github_user_fk_users_id;
+ALTER TABLE github_user ADD CONSTRAINT github_user_fk_github_id FOREIGN KEY (github_id)
+  REFERENCES users (github_id) ON DELETE NO ACTION ON UPDATE CASCADE DEFERRABLE;
+ALTER TABLE github_user DROP COLUMN access_token;
+ALTER TABLE github_user DROP COLUMN users_id;
 
 COMMIT;
