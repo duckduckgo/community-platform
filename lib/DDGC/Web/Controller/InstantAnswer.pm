46c26b09577697a6650a1fac9f5fabccaa2a51bd
Jason Dorweiler <jason@duckduckgo.com>
2015-12-17 09:45:31 -0500

    try all responses

--- lib/DDGC/Web/Controller/InstantAnswer.pm
+++ lib/DDGC/Web/Controller/InstantAnswer.pm
@@ -223,7 +223,11 @@ sub dev_pipeline_json :Chained('dev_pipeline_base') :PathPart('json') :Args(0) {
     my $asana_server = "http://beta.duckduckgo.com/install?asana&ia=everything";
 
     my $result = asana_req('', $asana_server);
-    $result = $result->decoded_content ? from_json($result->decoded_content) : undef;
+    try{
+        $result = $result->decoded_content ? from_json($result->decoded_content) : undef;
+    }catch{
+        $result = {};
+    };
 
     my %dev_ias;
     my $temp_ia;
@@ -768,11 +772,13 @@ sub ia_json :Chained('ia_base') :PathPart('json') :Args(0) {
         }
     }
 
-    my $server = "http://beta.duckduckgo.com/install?asana&ia=" . $ia->id;
-
+    my $server = "http://beta..duckduckgo.com/install?asana&ia=" . $ia->id;
     my $result = asana_req('', $server);
-    $ia_data{live}->{asana} = $result->decoded_content ? from_json($result->decoded_content) : undef;
-    $ia_data{live}->{asana} = $ia_data{live}->{asana}? $ia_data{live}->{asana}->{$ia->id} : undef;
+
+    try{
+        $ia_data{live}->{asana} = $result->decoded_content ? from_json($result->decoded_content) : undef;
+        $ia_data{live}->{asana} = $ia_data{live}->{asana}? $ia_data{live}->{asana}->{$ia->id} : undef;
+    };
 
     $c->stash->{x} = \%ia_data;
 
@@ -911,6 +917,12 @@ sub asana :Chained('base') :PathPart('asana') :Args(0) {
     my $server = "http://beta.duckduckgo.com/install?asana";
 
     my $result = asana_req(\%data, $server);
+    try{
+        $result = $result->decoded_content;
+    }
+    catch{
+        $result = {};
+    };
 
     $c->stash->{x}->{result} = $result->decoded_content;
     return $c->forward($c->view('JSON'));
