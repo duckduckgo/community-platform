b216a77682cfa20f584aa22d79183c36956b2812
John Barrett <john@jbrt.org>
2016-01-12 12:21:52 -0500

    Make DROPs / ALTERs conditional so you can switch between schema changes

--- sql/1.110/00_github_login.sql
+++ sql/1.110/00_github_login.sql
@@ -4,12 +4,10 @@ ALTER TABLE users RENAME COLUMN github_user TO github_user_plaintext;
 ALTER TABLE users ADD COLUMN github_id bigint;
 ALTER TABLE users ADD CONSTRAINT users_github_id UNIQUE (github_id);
 
-ALTER TABLE github_user DROP CONSTRAINT github_user_fk_users_id;
-ALTER TABLE github_user ADD CONSTRAINT github_user_fk_github_id FOREIGN KEY (github_id)
-  REFERENCES users (github_id) ON DELETE NO ACTION ON UPDATE CASCADE DEFERRABLE;
-ALTER TABLE github_user DROP COLUMN access_token;
-ALTER TABLE github_user DROP COLUMN users_id;
+ALTER TABLE github_user DROP CONSTRAINT IF EXISTS github_user_fk_users_id;
+ALTER TABLE github_user DROP COLUMN IF EXISTS access_token;
+ALTER TABLE github_user DROP COLUMN IF EXISTS users_id;
 
-DROP TABLE github_user_link;
+DROP TABLE IF EXISTS github_user_link;
 
 COMMIT;
--- sql/1.110/rollback/rollback.sql
+++ sql/1.110/rollback/rollback.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
 ALTER TABLE users RENAME COLUMN github_user_plaintext TO github_user;
+ALTER TABLE users DROP COLUMN github_id;
 
 COMMIT;
