7043e6a8e2e315896dc55c59c16f59617ae4c735
Maria Grazia Alastra <maria@duckduckgo.com>
2016-02-29 13:53:25 -0500

    maintainer_gh.pl - store is_live

--- sql/1.145/maintainer_gh.pl
+++ sql/1.145/maintainer_gh.pl
@@ -8,16 +8,17 @@ use DDGC;
 
 my $d  = DDGC->new;
 my $ias = $d->rs('InstantAnswer');
+my $is_live = $d->is_live;
 
 while (my $ia = $ias->next) {
     if (my $maintainer = $ia->maintainer) {
         my %updated_maintainer;
         my $duckco_user = $d->rs('User')->find({username => $maintainer});
 
-        if ($duckco_user || (!$d->is_live)) {
+        if ($duckco_user || (!$is_live)) {
             %updated_maintainer = ( duckco => $maintainer );
 
-            if ($d->is_live && $duckco_user->github_id) {
+            if ($is_live && $duckco_user->github_id) {
                 $updated_maintainer{github} = $d->rs('GitHub::User')->find({github_id => $duckco_user->github_id})->login;
             }
         } else {
