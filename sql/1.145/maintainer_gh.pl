267d0db304e265e9700dc4326943414f00c603d0
Maria Grazia Alastra <maria@duckduckgo.com>
2016-02-29 15:42:25 -0500

    maintainer_gh.pl - make sure we don't attempt to convert values that are already JSON strings

--- sql/1.145/maintainer_gh.pl
+++ sql/1.145/maintainer_gh.pl
@@ -3,6 +3,7 @@
 use FindBin;
 use lib "$FindBin::Dir/../../lib";
 use strict;
+use Try::Tiny;
 use JSON;
 use DDGC;
 
@@ -12,22 +13,27 @@ my $is_live = $d->is_live;
 
 while (my $ia = $ias->next) {
     if (my $maintainer = $ia->maintainer) {
-        my %updated_maintainer;
-        my $duckco_user = $d->rs('User')->find({username => $maintainer});
+        try {
+            my $temp_maintainer = from_json($maintainer);
+        } 
+        catch {
+            my %updated_maintainer;
+            my $duckco_user = $d->rs('User')->find({username => $maintainer});
 
-        if ($duckco_user || (!$is_live)) {
-            %updated_maintainer = ( duckco => $maintainer );
+            if ($duckco_user || (!$is_live)) {
+                %updated_maintainer = ( duckco => $maintainer );
 
-            if ($is_live && $duckco_user->github_id) {
-                $updated_maintainer{github} = $d->rs('GitHub::User')->find({github_id => $duckco_user->github_id})->login;
+                if ($is_live && $duckco_user->github_id) {
+                    $updated_maintainer{github} = $d->rs('GitHub::User')->find({github_id => $duckco_user->github_id})->login;
+                }
+            } else {
+                die "Maintainer '$maintainer' is not a duck.co user!";
             }
-        } else {
-            die "Maintainer '$maintainer' is not a duck.co user!";
-        }
 
-        if (%updated_maintainer) {
-            $maintainer = to_json \%updated_maintainer;
-            $ia->update({maintainer => $maintainer});
-        }
+            if (%updated_maintainer) {
+                $maintainer = to_json \%updated_maintainer;
+                $ia->update({maintainer => $maintainer});
+            }
+        };
     }
 }
