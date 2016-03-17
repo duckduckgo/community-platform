4f5a770f1b43927d571f9c66252d47cf38c6c312
olivia <olivia@duckduckgo.com>
2016-03-17 12:33:18 -0400

    refactored contributor_activity.sql - removed extraneous id and restricted FKs on delete

--- sql/1.107/contributor_activity.sql
+++ sql/1.107/contributor_activity.sql
@@ -1,12 +1,11 @@
 BEGIN;
 
 CREATE TABLE contributor_activity (
-    id serial NOT NULL PRIMARY KEY,
-    github_id text NOT NULL UNIQUE,
-    contributor_id integer NOT NULL REFERENCES github_user(id) ON DELETE CASCADE,
-    github_repo_id integer NOT NULL REFERENCES github_repo(id) ON DELETE CASCADE,
-    contributor_activity_type text NOT NULL,
-    contributor_activity_date timestamp with time zone  NOT NULL 
+    github_event_id text NOT NULL PRIMARY KEY,
+    contributor_id integer NOT NULL REFERENCES github_user(id) ON DELETE RESTRICT,
+    github_repo_id integer NOT NULL REFERENCES github_repo(id) ON DELETE RESTRICT,
+    contribution_type text NOT NULL,
+    contribution_date timestamp with time zone  NOT NULL 
 );
 
 COMMIT;
