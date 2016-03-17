aa2377a9d72cdf0af3c9943de4f825ecf8bc189d
olivia <olivia@duckduckgo.com>
2016-03-17 13:32:55 -0400

    moved and updated result set to reflect new contributor_activity table

---
+++ lib/DDGC/DB/Result/ContributorActivity.pm
@@ -0,0 +1,21 @@
+package DDGC::DB::Result::ContributorActivity;
+# ABSTRACT:
+
+use Moose;
+extends 'DDGC::DB::Base::Result';
+use DBIx::Class::Candy;
+use namespace::autoclean;
+
+table 'contributor_activity';
+
+primary_column github_event_id  => {data_type => 'text', is_nullable => 0};
+column contributor_id    => {data_type => 'bigint', is_nullable => 0};
+column github_repo_id    => {data_type => 'bigint', is_nullable => 0};
+column contribution_type => {data_type => 'text', is_nullable => 0};
+column contribution_date => {data_type => 'timestamp with time zone', is_nullable => 0};
+
+has_one 'repo', 'DDGC::DB::Result::GitHub::Repo', 'github_repo_id';
+has_one 'user', 'DDGC::DB::Result::GitHub::User', 'contributor_id';
+
+no Moose;
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
