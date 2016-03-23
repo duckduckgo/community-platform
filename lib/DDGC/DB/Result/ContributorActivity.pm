617a7053c93d3316cc472eeae9031911361f7fcb
olivia <olivia@duckduckgo.com>
2016-03-23 11:35:02 -0400

    trying alternate

--- lib/DDGC/DB/Result/ContributorActivity.pm
+++ lib/DDGC/DB/Result/ContributorActivity.pm
@@ -14,7 +14,7 @@ column github_repo_id           => {data_type => 'bigint', is_nullable => 0};
 column contribution_type        => {data_type => 'text', is_nullable => 0};
 column contribution_date        => {data_type => 'timestamp with time zone', is_nullable => 0};
 
-unique_constraint [qw( github_event_id github_repo_id)];
+primary_key [qw( github_event_id github_repo_id)];
 
 has_one 'repo', 'DDGC::DB::Result::GitHub::Repo', 'github_repo_id';
 has_one 'user', 'DDGC::DB::Result::GitHub::User', 'contributor_id';
