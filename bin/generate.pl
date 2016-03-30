fc7423dd86251cfa64668ed7efb3a84b6da05104
John Barrett <john@jbrt.org>
2016-03-30 11:21:06 -0400

    Launch markup.pl from generate.pl

--- bin/generate.pl
+++ bin/generate.pl
@@ -8,10 +8,15 @@ use lib $FindBin::Dir . "/../lib";
 
 use DDH::UserPage::Gather;
 use DDH::UserPage::Generate;
+use File::Spec::Functions;
+use IPC::Open3;
 
 DDH::UserPage::Generate->new(
     contributors => DDH::UserPage::Gather->new->contributors,
     view_dir     => "$FindBin::Dir/../views",
     build_dir    => "/home/ddgc/ddgc/ddh-userpages"
 )->generate;
+
+my($w, $r, $e);
+waitpid open3( $w, $r, $e, catfile( $FindBin::Dir, 'markup.pl' ) ), 0;
 
