53b48b34014c58c7eb66cf2c9333c16f58b52ae2
Jag Talon <jag@duckduckgo.com>
2016-03-30 10:27:32 -0400

    UserPage/Generate.pm: No content wrapping

--- lib/DDH/UserPage/Generate.pm
+++ lib/DDH/UserPage/Generate.pm
@@ -108,15 +108,16 @@ sub generate {
         );
 
         open my $fh, '>:encoding(UTF-8)', "$build_dir/index.html" or die();
-        print $fh $self->xslate->render(
-            'layouts/main.tx',
-            {
-                content => $content,
-                settings => $self->settings,
-                hide_login => 1,
-                no_js => 1
-            }
-        );
+        # print $fh $self->xslate->render(
+        #     'layouts/main.tx',
+        #     {
+        #         content => $content,
+        #         settings => $self->settings,
+        #         hide_login => 1,
+        #         no_js => 1
+        #     }
+        # );
+	print $fh $content;
         close $fh;
 
         # JSON dump of user's data
