d1f74d70d9406396fbde20efee8692cca6418088
Maria Grazia Alastra <maria@duckduckgo.com>
2016-04-20 14:49:10 -0400

    casperjs - test filtering by type

--- src/t/ia/index.js
+++ src/t/ia/index.js
@@ -4,10 +4,17 @@ casper.test.begin("IA Index", function suite(test) {
         casper.viewport(1336, 768).then(function() {
             this.reload(function() {
 
-                // check basic elements exist
+                test.comment("Check existence of basic elements in the page")
                 test.assertExists(".developer-nav");
                 test.assertExists("#ia_index");
                 test.assertExists("#filters");
+
+
+                test.comment("Test filtering by type - Goodies");
+                this.click("#ia_repo-goodies");
+
+                test.assertVisible(".ia-item > .ia_repo-goodies", "Goodies are visible after filtering");
+                test.assertNotVisible(".ia-item > .ia_repo-spice", "Spices are hidden after filtering");
 
           });
        });
