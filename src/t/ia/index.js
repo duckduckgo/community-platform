eb996e57b8245b4141722c3cfd1ef1154d19e828
Maria Grazia Alastra <maria@duckduckgo.com>
2016-04-20 14:20:05 -0400

    casperjs - demonstrate simple tests

--- src/t/ia/index.js
+++ src/t/ia/index.js
@@ -1,44 +1,19 @@
-/*var page = require('webpage').create();
-
-page.open('https://maria.duckduckgo.com:5001/ia', function(status) {
-    console.log(page.content);
-    phantom.exit();
-});
-*/
-
-
-casper.test.begin('IA Index', function suite(test) {
-    casper.start("https://www.github.com", function(response) {
+casper.test.begin("IA Index", function suite(test) {
+    casper.start("http://maria.duckduckgo.com:5001/ia", function(response) {
+        // we don't really support mobile yet
         casper.viewport(1336, 768).then(function() {
             this.reload(function() {
 
-            require('utils').dump(response);
-   
-            casper.echo(this.fetchText('h2'));
+                // check basic elements exist
+                test.assertExists(".developer-nav");
+                test.assertExists("#ia_index");
+                test.assertExists("#filters");
 
-            casper.echo(this.getPageContent());
-
-            casper.echo(this.userAgent());
-
-         });
-         });
-            
-    });
-        
-    casper.on('page.resource.received', function(requestData, request) {
-        casper.echo(JSON.stringify(requestData));
-    });
-
-    casper.then(function() {
-        this.echo(this.getHTML());
-    });
-    
-    casper.then(function() {
-            test.pass('Test');
+          });
+       });
     });
 
     casper.run(function() {
-        suite();
         test.done()
     });
 });
