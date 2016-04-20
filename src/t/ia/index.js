a91e1dfb2f4db948aa511f2047176808422018c9
Maria Grazia Alastra <maria@duckduckgo.com>
2016-04-20 13:04:31 -0400

    casperjs - dump headers for debugging reasons

--- src/t/ia/index.js
+++ src/t/ia/index.js
@@ -1,17 +1,18 @@
-var page = require('webpage').create();
+/*var page = require('webpage').create();
 
 page.open('https://maria.duckduckgo.com:5001/ia', function(status) {
     console.log(page.content);
     phantom.exit();
 });
+*/
 
 
-
-/*
 casper.test.begin('IA Index', function suite(test) {
-    casper.start("http://duck.co/ia", function() {
+    casper.start("http://duck.co/ia", function(response) {
         casper.viewport(1336, 768).then(function() {
             this.reload(function() {
+
+            require('utils').dump(response);
    
             casper.echo(this.fetchText('h2'));
 
@@ -42,8 +43,6 @@ casper.test.begin('IA Index', function suite(test) {
     });
 });
 
-i
 
 
 
-*/
