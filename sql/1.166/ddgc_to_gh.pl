1d079f0ebab8355a4367f009af53bce10e6cef9a
Maria Grazia Alastra <maria@duckduckgo.com>
2016-04-21 13:23:01 -0400

    ddgc_to_gh.pl - fix updating the db

--- sql/1.166/ddgc_to_gh.pl
+++ sql/1.166/ddgc_to_gh.pl
@@ -19,45 +19,46 @@ for my $ia ( @ias ) {
     warn "JSON dev column before update: " . $ia->developer;
     
     try {
-    if ( $ia->developer && ( $ia->developer ne '[]' ) ) {
-     
-        my $developers = from_json($ia->developer);
-        my $new_devs;
+        if ( $ia->developer && ( $ia->developer ne '[]' ) ) {
+         
+            my $developers = from_json($ia->developer);
+            my $new_devs;
 
-        for my $dev ( @{ $developers } ) {
-           
-            if ( ( ref $dev eq 'HASH' ) && $dev->{type} && ( $dev->{type} eq 'duck.co' ) ) {
+            for my $dev ( @{ $developers } ) {
+               
+                if ( ( ref $dev eq 'HASH' ) && $dev->{type} && ( $dev->{type} eq 'duck.co' ) ) {
 
-                warn "HASH";
-                p $dev;
-                if ( my $complat_user = $d->rs('User')->find({ username => $dev->{name} }) ) {
+                    warn "HASH";
+                    p $dev;
+                    if ( my $complat_user = $d->rs('User')->find({ username => $dev->{name} }) ) {
 
-                    my $gh_user = $complat_user->github_id ? $d->rs('GitHub::User')->find({ github_id => $complat_user->github_id }) : undef;
+                        my $gh_user = $complat_user->github_id ? $d->rs('GitHub::User')->find({ github_id => $complat_user->github_id }) : undef;
 
-                    if ( my $login = $gh_user ? $gh_user->login : $complat_user->github_user_plaintext ) {
+                        if ( my $login = $gh_user ? $gh_user->login : $complat_user->github_user_plaintext ) {
 
-                        warn $login;
+                            warn $login;
 
-                        my $new_dev = {
-                            name => $login,
-                            type => 'github',
-                            url => 'https://github.com/' . $login
-                        };
+                            my $new_dev = {
+                                name => $login,
+                                type => 'github',
+                                url => 'https://github.com/' . $login
+                            };
 
-                        push(@{ $new_devs }, $new_dev);
+                            push(@{ $new_devs }, $new_dev);
+                        }
                     }
+                } elsif ( $dev ) {
+                    warn "dev as it is";
+                    push(@{ $new_devs }, $dev);
                 }
-            } elsif ( $dev ) {
-                warn "dev as it is";
-                push(@{ $new_devs }, $dev);
             }
-        }
-        
-        
-        $developers = to_json($new_devs);
+            
+            if ( $new_devs ) { 
+                $developers = to_json($new_devs);
 
-        warn "JSON dev column updated: " . $developers;
-        $ia->update({ developer => $developers });
+                warn "JSON dev column updated: " . $developers;
+                $ia->update({ developer => $developers });
+            }
         }
     } catch {
         warn "error updating developers: " . $_;
