c5eb064cc8c182a15b3b360ad6fc12397a971156
Maria Grazia Alastra <maria@duckduckgo.com>
2016-04-27 12:25:42 -0400

    Gruntfile.js - update test-ia script path

--- Gruntfile.js
+++ Gruntfile.js
@@ -278,7 +278,7 @@ module.exports = function(grunt) {
             deleteBuildFiles: "mkdir -p build && rm -r build",
             bower: "bower install",
 	        commit_static: "git add root/static/* package.json && git commit -m 'Release IA pages version: <%= pkg.version %>'",
-            tests: "src/t/run.sh"
+            tests: "src/t/run.bash"
         },
 
 	// Build again if any of the JS / SCSS files changed.
