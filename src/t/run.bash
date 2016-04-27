4239c021e23519107481c931d03a465b421e7a76
Maria Grazia Alastra <maria@duckduckgo.com>
2016-04-27 13:41:26 -0400

    run.bash - travis uses a different path, so can't use an absolute path to find the script

--- src/t/run.bash
+++ src/t/run.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /etc/profile.d/perlbrew.sh
-plackup -p 3000 -s Starman -D /home/ddgc/community-platform/script/ddgc_dev_server.psgi
+
+plackup -p 3000 -s Starman -D script/ddgc_dev_server.psgi
 casperjs test src/t/ia/. --hostname=localhost:3000
