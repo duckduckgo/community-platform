888b06426aed732eea2eac6d8d0629d931658531
John Barrett <john@jbrt.org>
2016-04-28 11:02:22 -0400

    Explicitly bind plackup to IPv4 localhost

--- src/t/run.bash
+++ src/t/run.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 grunt
-plackup -p 3000 -s Starman -D script/ddgc_dev_server.psgi
+plackup --host 127.0.0.1 -p 3000 -s Starman -D script/ddgc_dev_server.psgi
 casperjs test src/t/ia/. --hostname=127.0.0.1:3000
