34505991388a1f2bf97a62a16956edecc4e41a81
John Barrett <john@jbrt.org>
2016-04-28 14:13:43 -0400

    Set DSN for test instance.

--- src/t/run.bash
+++ src/t/run.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 grunt
-plackup --host 127.0.0.1 -p 3000 -s Starman -D script/ddgc_dev_server.psgi
+DDGC_DB_DSN="dbi:SQLite:dbname=ddgc_test.db" plackup --host 127.0.0.1 -p 3000 -s Starman -D script/ddgc_dev_server.psgi --workers=5
 casperjs test src/t/ia/. --hostname=127.0.0.1:3000
