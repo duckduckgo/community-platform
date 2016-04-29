b74fe8af2853e12068e2a0f1017f6bc423eba526
John Barrett <john@jbrt.org>
2016-04-29 04:15:00 -0400

    Deploy test db in casper script

--- src/t/run.bash
+++ src/t/run.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+script/ddgc_deploy_test_db.pl
 export PIDFILE="/tmp/ddgc-$(date --iso-8601=seconds).pid"
 grunt
 DDGC_DB_DSN="dbi:SQLite:dbname=ddgc_test.db" plackup --host 127.0.0.1 -p 3000 -s Starman -D --pid $PIDFILE script/ddgc_dev_server.psgi
