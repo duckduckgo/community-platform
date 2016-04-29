117c4ea9b32e0842135f0bce4157034dffd6faa4
John Barrett <john@jbrt.org>
2016-04-29 03:21:35 -0400

    Cleanup test server process

--- src/t/run.bash
+++ src/t/run.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PIDFILE="/tmp/ddgc-$(date --iso-8601=seconds).pid"
 grunt
-DDGC_DB_DSN="dbi:SQLite:dbname=ddgc_test.db" plackup --host 127.0.0.1 -p 3000 -s Starman -D script/ddgc_dev_server.psgi --workers=5
+DDGC_DB_DSN="dbi:SQLite:dbname=ddgc_test.db" plackup --host 127.0.0.1 -p 3000 -s Starman -D --pid $PIDFILE script/ddgc_dev_server.psgi
 casperjs test src/t/ia/. --hostname=127.0.0.1:3000
+kill $(cat $PIDFILE)
