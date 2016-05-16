d6f467b5386e430416efa759a5b7ece9f8b32f48
Ben Moon <guiltydolphin@gmail.com>
2016-05-16 22:08:40 +0100

    Improve randomness of data
    Actually generate *random* traffic!

--- script/ddgc_generate_traffic.pl
+++ script/ddgc_generate_traffic.pl
@@ -26,18 +26,29 @@ my $today = DateTime->now();
 my $last_month = $today->clone()->subtract(days => 31);
 
 my @days = map { $last_month->clone->add(days => $_)->strftime('%F') } (1..31);
-my @counts = (1..31);
 my $days = \@days;
-my $counts = \@counts;
+
+sub generate_bounded_counts {
+    my ($min, $max) = @_;
+    map { int(rand($max+$min)) + $min } (1..31);
+}
+
+my %bounds = (
+    small => [0, 3_000],
+    large => [1_000, 100_000],
+);
+
+# Returns a month's worth of counts
+sub get_random_counts {
+    generate_bounded_counts(int(rand())
+        ? @{$bounds{small}} : @{$bounds{large}}
+    );
+}
 
 foreach my $id (@ids) {
-    foreach (pairs zip @days, @counts) {
-        my ($day, $count) = @$_;
-        $d->rs('InstantAnswer::Traffic')->update_or_create({
-            answer_id  => $id,
-            date       => $day,
-            count      => $count,
-            pixel_type => 'iaoi',
-        });
-    }
+    my @counts = get_random_counts();
+    $d->rs('InstantAnswer::Traffic')->populate([
+        [qw( answer_id pixel_type date count )],
+        map { my ($date, $count) = @$_; [$id, 'iaoi', $date, $count] } pairs zip (@days, @counts),
+    ]);
 }
