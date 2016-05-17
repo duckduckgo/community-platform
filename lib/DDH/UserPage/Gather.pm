1988a4fa67293a7638b949291d12d5ffe31fb015
Maria Grazia Alastra <maria@duckduckgo.com>
2016-05-17 14:33:59 -0400

    Gather.pm - get maintainer avatar for other issues and prs

--- lib/DDH/UserPage/Gather.pm
+++ lib/DDH/UserPage/Gather.pm
@@ -153,6 +153,13 @@ sub find_ia {
 
         if ( my $ia = $self->ddgc->rs('InstantAnswer')->find({ meta_id => $ia_issue->instant_answer_id }) ) {
             $issue->{ia_name} = $ia->name;
+
+            if ( my $login = $ia->maintainer? $ia->maintainer->github : undef ) {
+                $issue->{maintainer} = {
+                    username => $login,
+                    avatar_url => $self->get_avatar($login)
+                }
+            }
         }
     }
 
