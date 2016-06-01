286d2a0883bfc36e4352f3cc778db568e4dd432b
Maria Grazia Alastra <maria@duckduckgo.com>
2016-06-01 14:29:39 -0400

    code.sql - fix data for R Cheat Sheet

--- sql/1.170/code.sql
+++ sql/1.170/code.sql
@@ -1,4 +1,6 @@
 BEGIN;
 UPDATE instant_answer SET code='["share/spice/time/capitals.yml","share/spice/time/content.handlebars","share/spice/time/time.css","share/spice/time/time.js","t/Time.t","lib/DDG/Spice/Time.pm"]' 
 WHERE meta_id='time';
+UPDATE instant_answer SET code='["share/goodie/cheat_sheets/json/r.json","lib/DDG/Goodie/CheatSheets.pm","t/CheatSheets/CheatSheets.t"]' 
+WHERE meta_id='r_cheat_sheet';
 COMMIT;
