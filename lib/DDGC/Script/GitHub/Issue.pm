d8ddb21c7ee0475f1f0b014457380fd270efd7d7
Ben Moon <guiltydolphin@gmail.com>
2016-09-08 16:43:49 +0100

    Clarify IA Page regexes

--- lib/DDGC/Script/GitHub/Issue.pm
+++ lib/DDGC/Script/GitHub/Issue.pm
@@ -14,17 +14,18 @@ sub get_mentions {
     return [@mentions];
 }
 
+my $ia_re = qr/(?:Instant\s?Answer|IA)\sPage/i;
+my $duck_co_re = qr{https?://duck\.co/ia/view/(?<id>\w+[^\s])}i;
 # get the IA name from the link in the first comment
 # Update this later for whatever format we decide on
 # Match (roughly) the following formats:
 # Instant Answer Page: Link   <- preferred standard link
+my $norm_form = qr{$ia_re:?\s+$duck_co_re}i;
 # [Instant Answer Page](Link) <- GHFM link
+my $gh_form = qr{\[$ia_re\]\($duck_co_re\)}i;
 sub id_from_body {
     my ($body) = @_;
-    $body =~ qr{
-    \[(?:Instant\s?Answer|IA)\sPage\]\(https?://duck\.co/ia/view/(?<id>\w+[^\s])\)
-    | (?:Instant\s?Answer|IA)\sPage:?\s+ https?://duck\.co/ia/view/(?<id>\w+[^\s])}ix;
-
+    $body =~ qr{$gh_form|$norm_form}i;
     return $+{id};
 }
 
