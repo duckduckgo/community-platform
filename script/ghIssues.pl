6150a1f7cccc5833c16f25fee400a97479a57def
Ben Moon <guiltydolphin@gmail.com>
2016-09-08 15:10:17 +0100

    Pull Perl module grabber into separate sub

--- script/ghIssues.pl
+++ script/ghIssues.pl
@@ -176,19 +176,8 @@ sub getIssues{
 
                 my $template = find_template(\@files_data);
 
-                my $pm;
                 # look for the perl module
-                for my $file (@files_data){
-                    my $tmp_repo = ucfirst $data->{repo};
-                    $tmp_repo =~ s/s$//g;
-
-                    if(my ($name) = $file->{filename} =~ /lib\/DDG\/$tmp_repo\/(.+)\.pm/i ){
-                        my @parts = split('/', $name);
-                        $name = join('::', @parts);
-                        $pm = "DDG::".$tmp_repo."::$name";
-                        last;
-                    }
-                }
+                my $pm = perl_module_from_files($data->{repo}, \@files_data);
 
                 my $is_new_ia;
                 for my $tag (@{$data->{tags}}){
@@ -277,6 +266,21 @@ sub developer_from_author {
             type => 'github',
             url  => "https://github.com/$author"
         }];
+}
+
+sub perl_module_from_files {
+    my ($repo, $files_data) = @_;
+    foreach my $file (@{$files_data}) {
+        my $tmp_repo = ucfirst $repo;
+        $tmp_repo =~ s/s$//g;
+
+        if(my ($name) = $file->{filename} =~ /lib\/DDG\/$tmp_repo\/(.+)\.pm/i ){
+            my @parts = split('/', $name);
+            $name = join('::', @parts);
+            return "DDG::${tmp_repo}::$name";
+        }
+    }
+    return;
 }
 
 sub get_last_commit {
