70171abe0ede1d54b7f95d35d224eef718dd1825
John Barrett <john@jbrt.org>
2017-01-16 10:56:07 -0500

    Form / POST URLs for submitting addresses for test run
    - Should not perform the run for non-DDG addresses

--- lib/DDGC/Web/App/Subscriber.pm
+++ lib/DDGC/Web/App/Subscriber.pm
@@ -3,6 +3,7 @@ package DDGC::Web::App::Subscriber;
 # ABSTRACT: Subscriber management
 
 use DDGC::Base::Web::Light;
+use DDGC::Util::Script::SubscriberMailer;
 use Email::Valid;
 
 get '/u/:campaign/:email/:key' => sub {
@@ -42,6 +43,29 @@ get '/form' => sub {
         <input type="hidden" name="flow" value="form">
     </form>
 FORM
+};
+
+get '/testrun/:campaign' => sub {
+    return <<'TESTRUN'
+    <form method="POST">
+        <h3>Send a test run of all mails</h3>
+        email: <input type="text" name="email">
+        <input type="submit" name="submit">
+    </form>
+TESTRUN
+};
+
+post '/testrun/:campaign' => sub {
+    my $routeparams = params('route');
+    my $bodyparams = params('body');
+    my $email = Email::Valid->address($bodyparams->{email});
+    return unless $email;
+    return unless $email =~ /\@duckduckgo\.com$/;
+    DDGC::Util::Script::SubscriberMailer->new->testrun(
+        $routeparams->{campaign},
+        $bodyparams->{email},
+    );
+    return 'OK';
 };
 
 post '/a' => sub {
