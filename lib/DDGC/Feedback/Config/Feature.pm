c33c321affccd3e2f21d561af14f4169586bb6da
Daniel Davis <github@kyokodaniel.com>
2017-10-31 18:21:01 +0900

    Removed links to old forum

--- lib/DDGC/Feedback/Config/Feature.pm
+++ lib/DDGC/Feedback/Config/Feature.pm
@@ -9,12 +9,8 @@ sub feedback_title { "I've got a feature request." }
 sub feedback {[
   { description => "We'd love your suggestions!. Please check our <a href='https://duck.co/help'>Help pages</a> for common requests like making an email service or a browser. If you can't find what you're looking for, use the options below:", type => "info", icon => "newspaper", },
 
-  { description => "It's a feature that could be an Instant Answer", icon => "sun" },
-
-    "Please submit Instant Answer ideas to our community voting platform at <a href='https://duck.co/ideas'>https://duck.co/ideas</a>. If you're a developer, you can even make them yourself at <a href='http://duckduckhack.com/'>http://duckduckhack.com/</a>.",
-
-  { description => "It's a feature that wouldn’t work as an Instant Answer", icon => "coffee" },
-    "Please submit your idea to our community forum so that others can help expand (or even develop) your idea: <a href='https://duck.co/forum'>https://duck.co/forum</a>",
+  { description => "It's a feature", icon => "sun" },
+    "Please share your idea with the DuckDuckGo community on our subreddit: <a href='https://www.reddit.com/r/duckduckgo/'>reddit.com/r/duckduckgo</a>",
 
   { description => "It's a translation request", icon => "globe" },
     "You can translate DuckDuckGo to your language through the <a href='https://duck.co/translate'>Community Platform</a>. If you don't see your language available, please request it <a href='https://duck.co/my/requestlanguage'>here</a>.",
